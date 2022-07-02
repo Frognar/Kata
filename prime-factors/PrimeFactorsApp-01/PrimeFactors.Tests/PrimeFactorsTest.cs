ed0452534d602676ba0350fd56dc52a079116a78
Frognar <sebastian.przyszlak@gmail.com>
2022-07-02 09:44:41 +0200

    3 -> [3]

--- prime-factors/PrimeFactorsApp-01/PrimeFactors.Tests/PrimeFactorsTest.cs
+++ prime-factors/PrimeFactorsApp-01/PrimeFactors.Tests/PrimeFactorsTest.cs
@@ -6,5 +6,6 @@ public class PrimeFactorsTest {
     PrimeFactors primeFactors = new();
     Assert.Equal(Enumerable.Empty<int>(), primeFactors.Of(1));
     Assert.Equal(new List<int> { 2 }, primeFactors.Of(2));
+    Assert.Equal(new List<int> { 3 }, primeFactors.Of(3));
   }
 }
