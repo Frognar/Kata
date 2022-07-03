a83c7bd9fd57750b07498f43ead1c1c10a811d6e
Frognar <sebastian.przyszlak@gmail.com>
2022-07-03 08:50:02 +0200

    GetPrimesUpTo 100 -> [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]

--- prime-factors/PrimeFactorsApp-02/PrimeFactorsTests/PrimeFactorsTest.cs
+++ prime-factors/PrimeFactorsApp-02/PrimeFactorsTests/PrimeFactorsTest.cs
@@ -25,6 +25,7 @@ public class PrimeFactorsTest {
     Assert.Equal(new List<int> { 2 }, generator.GetPrimesUpTo(2));
     Assert.Equal(new List<int> { 2, 3 }, generator.GetPrimesUpTo(3));
     Assert.Equal(new List<int> { 2, 3 }, generator.GetPrimesUpTo(4));
+    Assert.Equal(new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 }, generator.GetPrimesUpTo(100));
   }
 
   [Fact]
