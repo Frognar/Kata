5df2b823e6e2502b0c0a3032b4c335607110a506
Frognar <sebastian.przyszlak@gmail.com>
2022-07-03 09:00:31 +0200

    Prime factors using sieve

--- prime-factors/PrimeFactorsApp-02/PrimeFactorsTests/PrimeFactorsTest.cs
+++ prime-factors/PrimeFactorsApp-02/PrimeFactorsTests/PrimeFactorsTest.cs
@@ -30,17 +30,16 @@ public class PrimeFactorsTest {
 
   [Fact]
   public void FactorsOfTest() {
-    PrimeGenerator generator = new TestPrimeGenerator();
+    PrimeGenerator generator = new Sieve();
     PrimeFactors primeFactors = new(generator);
     Assert.Equal(Enumerable.Empty<int>(), primeFactors.Of(1));
     Assert.Equal(new List<int> { 2 }, primeFactors.Of(2));
     Assert.Equal(new List<int> { 3 }, primeFactors.Of(3));
     Assert.Equal(new List<int> { 2, 2 }, primeFactors.Of(4));
-  }
-
-  class TestPrimeGenerator : PrimeGenerator {
-    public IEnumerable<int> GetPrimesUpTo(int n) {
-      return new List<int> { 2, 3 };
-    }
+    Assert.Equal(new List<int> { 5 }, primeFactors.Of(5));
+    Assert.Equal(new List<int> { 2, 3 }, primeFactors.Of(6));
+    Assert.Equal(new List<int> { 7 }, primeFactors.Of(7));
+    Assert.Equal(new List<int> { 2, 2, 2 }, primeFactors.Of(8));
+    Assert.Equal(new List<int> { 3, 3 }, primeFactors.Of(9));
   }
 }
