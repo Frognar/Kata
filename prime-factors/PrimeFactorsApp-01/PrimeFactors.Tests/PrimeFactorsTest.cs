9d5293825a3cc19b25c2132fba3340fa4646f776
Frognar <sebastian.przyszlak@gmail.com>
2022-07-03 09:39:41 +0200

    90499 -> [90499]

--- prime-factors/PrimeFactorsApp-01/PrimeFactors.Tests/PrimeFactorsTest.cs
+++ prime-factors/PrimeFactorsApp-01/PrimeFactors.Tests/PrimeFactorsTest.cs
@@ -14,5 +14,6 @@ public class PrimeFactorsTest {
     Assert.Equal(new List<int> { 2, 2, 2 }, primeFactors.Of(8));
     Assert.Equal(new List<int> { 3, 3 }, primeFactors.Of(9));
     Assert.Equal(new List<int> { 2, 2, 3, 5, 7, 11, 11, 13 }, primeFactors.Of(2 * 2 * 3 * 5 * 7 * 11 * 11 * 13));
+    Assert.Equal(new List<int> { 90499 }, primeFactors.Of(90499));
   }
 }
