7394c5d71f1fcf08c379065ceeda4d671d0fa4f9
Frognar <sebastian.przyszlak@gmail.com>
2022-07-03 09:21:48 +0200

    Sieve refactor

--- prime-factors/PrimeFactorsApp-02/PrimeFactors/Sieve.cs
+++ prime-factors/PrimeFactorsApp-02/PrimeFactors/Sieve.cs
@@ -2,32 +2,24 @@
 
 public class Sieve : PrimeGenerator {
   public IEnumerable<int> GetPrimesUpTo(int n) {
-    List<int> primes = new();
-    IEnumerable<int> candidates = GetCandidatesUpTo(n);
-    if (candidates.Any()) {
-      for (int first = candidates.First();
-        first > 0 && first <= n;
-        candidates = candidates.Except(GetMultiplesOf(first, n)).ToList(),
-        first = candidates.FirstOrDefault()) {
-        primes.Add(first);
+    HashSet<int> noPrimes = GetNoPrimesUpTo(n);
+    for (int i = 2; i <= n; i++) {
+      if (noPrimes.Contains(i) == false) {
+        yield return i;
+      }
+    }
+  }
+
+  static HashSet<int> GetNoPrimesUpTo(int n) {
+    HashSet<int> noPrimes = new();
+    for (int i = 2; i <= n; i++) {
+      for (int j = i * 2; j <= n; j += i) {
+        if (noPrimes.Contains(j) == false) {
+          noPrimes.Add(j);
+        }
       }
     }
 
-    return primes;
-  }
-
-  static IEnumerable<int> GetCandidatesUpTo(int n) {
-    List<int> candidates = new();
-    for (int i = 2; i <= n; i++) {
-      candidates.Add(i);
-    }
-
-    return candidates;
-  }
-
-  static IEnumerable<int> GetMultiplesOf(int n, int limit) {
-    for (int multiple = n; multiple <= limit; multiple += n) {
-      yield return multiple;
-    } 
+    return noPrimes;
   }
 }
