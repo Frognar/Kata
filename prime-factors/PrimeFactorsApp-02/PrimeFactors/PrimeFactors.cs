0937225ca3cf5b280e4c3e6f1535d5ee0ca451c0
Frognar <sebastian.przyszlak@gmail.com>
2022-07-03 07:44:06 +0200

    PrimeFactora with PrimeGenerator field

--- prime-factors/PrimeFactorsApp-02/PrimeFactors/PrimeFactors.cs
+++ prime-factors/PrimeFactorsApp-02/PrimeFactors/PrimeFactors.cs
@@ -1,6 +1,12 @@
 ﻿namespace PrimeFactors;
 
 public class PrimeFactors {
+  readonly PrimeGenerator generator;
+
+  public PrimeFactors(PrimeGenerator generator) {
+    this.generator = generator;
+  }
+
   public IEnumerable<int> Of(int n) {
     List<int> factors = new();
     if (n > 1) {
--- prime-factors/PrimeFactorsApp-02/PrimeFactorsTests/PrimeFactorsTest.cs
+++ prime-factors/PrimeFactorsApp-02/PrimeFactorsTests/PrimeFactorsTest.cs
@@ -4,7 +4,7 @@ public class PrimeFactorsTest {
   [Fact]
   public void FactorsOfTest() {
     PrimeGenerator generator = new TestPrimeGenerator();
-    PrimeFactors primeFactors = new();
+    PrimeFactors primeFactors = new(generator);
     Assert.Equal(Enumerable.Empty<int>(), primeFactors.Of(1));
     Assert.Equal(new List<int> { 2 }, primeFactors.Of(2));
     Assert.Equal(new List<int> { 3 }, primeFactors.Of(3));
