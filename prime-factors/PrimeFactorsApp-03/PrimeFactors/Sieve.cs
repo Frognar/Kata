91d595bb2947c9995d84cfc6d26f38e8175598c4
Frognar <sebastian.przyszlak@gmail.com>
2022-07-04 14:17:37 +0200

    refactor

--- prime-factors/PrimeFactorsApp-03/PrimeFactors/Sieve.cs
+++ prime-factors/PrimeFactorsApp-03/PrimeFactors/Sieve.cs
@@ -2,15 +2,8 @@
 
 public class Sieve : PrimeGenerator {
   public IEnumerable<long> GetPrimesUpTo(long n) {
-    if (n < 2) {
-      return Enumerable.Empty<long>();
-    }
-
-    HashSet<long> primes = new();
     for (int i = 2; i <= n; i++) {
-      primes.Add(i);
+      yield return i;
     }
-
-    return primes;
   }
 }
