5f7eae1f7341b190196faf968d60de7501bb70ab
Frognar <sebastian.przyszlak@gmail.com>
2022-07-04 14:37:39 +0200

    3 -> [3]

--- prime-factors/PrimeFactorsApp-03/PrimeFactors.Tests/PrimeFactorsTests.cs
+++ prime-factors/PrimeFactorsApp-03/PrimeFactors.Tests/PrimeFactorsTests.cs
@@ -6,5 +6,6 @@ public class PrimeFactorsTests {
     PrimeFactors primeFactors = new();
     Assert.Empty(primeFactors.Of(1));
     Assert.Equal(new List<long> { 2 }, primeFactors.Of(2));
+    Assert.Equal(new List<long> { 3 }, primeFactors.Of(3));
   }
 }
