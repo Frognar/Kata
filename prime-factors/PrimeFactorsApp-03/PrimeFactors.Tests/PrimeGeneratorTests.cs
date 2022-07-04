ba2797d0e44be19b0152b8395682899dddbcec46
Frognar <sebastian.przyszlak@gmail.com>
2022-07-04 14:19:46 +0200

    primes up to 4 -> [2, 3]

--- prime-factors/PrimeFactorsApp-03/PrimeFactors.Tests/PrimeGeneratorTests.cs
+++ prime-factors/PrimeFactorsApp-03/PrimeFactors.Tests/PrimeGeneratorTests.cs
@@ -7,5 +7,6 @@ public class PrimeGeneratorTests {
     Assert.Empty(generator.GetPrimesUpTo(1));
     Assert.Equal(new List<long> { 2 }, generator.GetPrimesUpTo(2));
     Assert.Equal(new List<long> { 2, 3 }, generator.GetPrimesUpTo(3));
+    Assert.Equal(new List<long> { 2, 3 }, generator.GetPrimesUpTo(4));
   }
 }
--- prime-factors/PrimeFactorsApp-03/PrimeFactors/Sieve.cs
+++ prime-factors/PrimeFactorsApp-03/PrimeFactors/Sieve.cs
@@ -2,8 +2,13 @@
 
 public class Sieve : PrimeGenerator {
   public IEnumerable<long> GetPrimesUpTo(long n) {
+    HashSet<long> primes = new();
     for (int i = 2; i <= n; i++) {
-      yield return i;
+      if (primes.All(p => i % p != 0)) {
+        primes.Add(i);
+      }
     }
+
+    return primes;
   }
 }
