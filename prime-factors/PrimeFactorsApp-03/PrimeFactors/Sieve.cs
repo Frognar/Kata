3c10819f6e69551d529de7089fdf8e6298b2e5a3
Frognar <sebastian.przyszlak@gmail.com>
2022-07-04 14:32:00 +0200

    refactor

--- prime-factors/PrimeFactorsApp-03/PrimeFactors/Sieve.cs
+++ prime-factors/PrimeFactorsApp-03/PrimeFactors/Sieve.cs
@@ -1,11 +1,19 @@
 ﻿namespace PrimeFactors;
 
 public class Sieve : PrimeGenerator {
-  public IEnumerable<long> GetPrimesUpTo(long n) {
-    HashSet<long> primes = new();
-    for (int i = 2; i <= n; i++) {
-      if (primes.All(p => i % p != 0)) {
-        primes.Add(i);
+  public IEnumerable<long> GetPrimesUpTo(long n)
+    => n < 2 ? Enumerable.Empty<long>() : PrimesUpTo(n);
+
+  static IEnumerable<long> PrimesUpTo(long n) {
+    List<long> primes = new();
+    bool[] composites = new bool[n + 1];
+
+    for (long candidate = 2; candidate <= n; candidate++) {
+      if (composites[candidate] == false) {
+        primes.Add(candidate);
+        for (long multiple = 2 * candidate; multiple <= n; multiple += candidate) {
+          composites[multiple] = true;
+        }
       }
     }
 
