041d9d43cc876ac2902b3c1c8e60922b3fdc9d80
Frognar <sebastian.przyszlak@gmail.com>
2022-07-04 14:47:38 +0200

    refactor

--- prime-factors/PrimeFactorsApp-03/PrimeFactors.Tests/PrimeFactorsTests.cs
+++ prime-factors/PrimeFactorsApp-03/PrimeFactors.Tests/PrimeFactorsTests.cs
@@ -3,7 +3,7 @@
 public class PrimeFactorsTests {
   [Fact]
   public void PrimeFactorsTest() {
-    PrimeFactors primeFactors = new();
+    PrimeFactors primeFactors = new(new Sieve());
     Assert.Empty(primeFactors.Of(1));
     Assert.Equal(new List<long> { 2 }, primeFactors.Of(2));
     Assert.Equal(new List<long> { 3 }, primeFactors.Of(3));
@@ -13,5 +13,10 @@ public class PrimeFactorsTests {
     Assert.Equal(new List<long> { 7 }, primeFactors.Of(7));
     Assert.Equal(new List<long> { 2, 2, 2 }, primeFactors.Of(8));
     Assert.Equal(new List<long> { 3, 3 }, primeFactors.Of(9));
+    Assert.Equal(new List<long> { 2, 2, 3, 5, 7, 11 }, primeFactors.Of(2 * 2 * 3 * 5 * 7 * 11));
+    Assert.Equal(new List<long> { 2, 2, 3, 5, 7, 11, 11, 13 }, primeFactors.Of(2 * 2 * 3 * 5 * 7 * 11 * 11 * 13));
+    Assert.Equal(new List<long> { 90499 }, primeFactors.Of(90499));
+    Assert.Equal(new List<long> { (long)Math.Pow(2, 31) - 1 }, primeFactors.Of((long)Math.Pow(2, 31) - 1));
+    //Assert.Equal(new List<long> { (long)Math.Pow(2, 61) - 1 }, primeFactors.Of((long)Math.Pow(2, 61) - 1));
   }
 }
--- prime-factors/PrimeFactorsApp-03/PrimeFactors/PrimeFactors.cs
+++ prime-factors/PrimeFactorsApp-03/PrimeFactors/PrimeFactors.cs
@@ -1,12 +1,27 @@
 ﻿namespace PrimeFactors;
 
 public class PrimeFactors {
+  readonly PrimeGenerator generator;
+
+  public PrimeFactors(PrimeGenerator generator) {
+    this.generator = generator;
+  }
+
   public IEnumerable<long> Of(long n) {
+    if (n < 2) {
+      return Enumerable.Empty<long>();
+    }
+
+    long limit = (long)Math.Sqrt(n) + 1;
     List<long> factors = new();
-    for (long divisior = 2; n > 1; divisior++) {
-      for (; n % divisior == 0; n /= divisior) {
-        factors.Add(divisior);
+    foreach (long prime in generator.GetPrimesUpTo(limit)) {
+      for (; n % prime == 0; n /= prime) {
+        factors.Add(prime);
       }
+    }
+
+    if (factors.Count == 0) {
+      factors.Add(n);
     }
 
     return factors;
