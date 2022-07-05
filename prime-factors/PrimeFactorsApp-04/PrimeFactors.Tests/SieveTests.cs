8bdf0ad13c725d53040c2e11e65e63bf2b54c2f1
Frognar <sebastian.przyszlak@gmail.com>
2022-07-05 15:37:48 +0200

    more tests

--- prime-factors/PrimeFactorsApp-04/PrimeFactors.Tests/SieveTests.cs
+++ prime-factors/PrimeFactorsApp-04/PrimeFactors.Tests/SieveTests.cs
@@ -7,5 +7,13 @@ public class SieveTests {
     Assert.Empty(sieve.PrimesUpTo(1L));
     Assert.Equal(new List<long> { 2 }, sieve.PrimesUpTo(2));
     Assert.Equal(new List<long> { 2, 3 }, sieve.PrimesUpTo(3));
+    Assert.Equal(new List<long> { 2, 3 }, sieve.PrimesUpTo(4));
+    Assert.Equal(new List<long> { 2, 3, 5 }, sieve.PrimesUpTo(5));
+    Assert.Equal(new List<long> { 2, 3, 5 }, sieve.PrimesUpTo(6));
+    Assert.Equal(new List<long> { 2, 3, 5, 7 }, sieve.PrimesUpTo(7));
+    Assert.Equal(new List<long> { 2, 3, 5, 7 }, sieve.PrimesUpTo(8));
+    Assert.Equal(new List<long> { 2, 3, 5, 7 }, sieve.PrimesUpTo(9));
+    Assert.Equal(new List<long> { 2, 3, 5, 7 }, sieve.PrimesUpTo(10));
+    Assert.Equal(new List<long> { 2, 3, 5, 7, 11 }, sieve.PrimesUpTo(11));
   }
 }
