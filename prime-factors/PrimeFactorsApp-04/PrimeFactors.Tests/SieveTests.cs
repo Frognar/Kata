49c469571ad882000d77bdff2328caf3a9dd4b1e
Frognar <sebastian.przyszlak@gmail.com>
2022-07-05 15:36:51 +0200

    3 -> [2, 3]

--- prime-factors/PrimeFactorsApp-04/PrimeFactors.Tests/SieveTests.cs
+++ prime-factors/PrimeFactorsApp-04/PrimeFactors.Tests/SieveTests.cs
@@ -6,6 +6,6 @@ public class SieveTests {
     Sieve sieve = new();
     Assert.Empty(sieve.PrimesUpTo(1L));
     Assert.Equal(new List<long> { 2 }, sieve.PrimesUpTo(2));
-    //Assert.Equal(new List<long> { 2, 3 }, sieve.PrimesUpTo(3));
+    Assert.Equal(new List<long> { 2, 3 }, sieve.PrimesUpTo(3));
   }
 }
--- prime-factors/PrimeFactorsApp-04/PrimeFactors/Sieve.cs
+++ prime-factors/PrimeFactorsApp-04/PrimeFactors/Sieve.cs
@@ -6,7 +6,17 @@ public class Sieve {
       return Enumerable.Empty<long>();
     }
 
-    List<long> primes = new() { 2 };
+    bool[] composites = new bool[n + 1];
+    List<long> primes = new();
+    for (long candidate = 2; candidate <= n; candidate++) {
+      if (composites[candidate] == false) {
+        primes.Add(candidate);
+        for (long multiply = 2 * candidate; multiply <= n; multiply += candidate) {
+          composites[multiply] = true;
+        }
+      }
+    }
+
     return primes;
   }
 }
