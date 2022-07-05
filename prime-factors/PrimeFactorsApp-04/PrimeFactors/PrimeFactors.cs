9940762396440be4baa76142c221f4c78bf6fd0a
Frognar <sebastian.przyszlak@gmail.com>
2022-07-05 15:27:01 +0200

    4 -> [2, 2]

--- prime-factors/PrimeFactorsApp-04/PrimeFactors/PrimeFactors.cs
+++ prime-factors/PrimeFactorsApp-04/PrimeFactors/PrimeFactors.cs
@@ -3,6 +3,14 @@
 public class PrimeFactors {
   public IEnumerable<long> Of(long n) {
     List<long> factors = new();
+    long prime = 2;
+    if (n > 1) {
+      if (n % prime == 0) {
+        factors.Add(prime);
+        n /= prime;
+      }
+    }
+    
     if (n > 1) {
       factors.Add(n);
     }
