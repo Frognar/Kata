43f1903525a83554efc8daed3f5852ddb278082e
Frognar <sebastian.przyszlak@gmail.com>
2022-07-06 16:07:26 +0200

    Prime factors created with sieve

--- prime-factors/PrimeFactorsApp-05/PrimeFactors/PrimeFactors.cs
+++ prime-factors/PrimeFactorsApp-05/PrimeFactors/PrimeFactors.cs
@@ -1,6 +1,12 @@
 ﻿namespace PrimeFactors;
 
 public class PrimeFactors {
+  readonly Sieve sieve;
+
+  public PrimeFactors(Sieve sieve) {
+    this.sieve = sieve;
+  }
+
   public IEnumerable<long> Of(long n) {
     List<long> factors = new();
     long prime = 2;
