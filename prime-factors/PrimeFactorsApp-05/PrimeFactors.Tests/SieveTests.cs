2fc69ad693ecc4ac14c125e63f9fbc62ddda9a8c
Frognar <sebastian.przyszlak@gmail.com>
2022-07-06 16:06:50 +0200

    p 11 -> 2, 3, 5, 7, 11

--- prime-factors/PrimeFactorsApp-05/PrimeFactors.Tests/SieveTests.cs
+++ prime-factors/PrimeFactorsApp-05/PrimeFactors.Tests/SieveTests.cs
@@ -8,5 +8,12 @@ public class SieveTests {
     Assert.Equal(new List<long> { 2 }, sieve.GetPrimesUpTo(2));
     Assert.Equal(new List<long> { 2, 3 }, sieve.GetPrimesUpTo(3));
     Assert.Equal(new List<long> { 2, 3 }, sieve.GetPrimesUpTo(4));
+    Assert.Equal(new List<long> { 2, 3, 5 }, sieve.GetPrimesUpTo(5));
+    Assert.Equal(new List<long> { 2, 3, 5 }, sieve.GetPrimesUpTo(6));
+    Assert.Equal(new List<long> { 2, 3, 5, 7 }, sieve.GetPrimesUpTo(7));
+    Assert.Equal(new List<long> { 2, 3, 5, 7 }, sieve.GetPrimesUpTo(8));
+    Assert.Equal(new List<long> { 2, 3, 5, 7 }, sieve.GetPrimesUpTo(9));
+    Assert.Equal(new List<long> { 2, 3, 5, 7 }, sieve.GetPrimesUpTo(10));
+    Assert.Equal(new List<long> { 2, 3, 5, 7, 11 }, sieve.GetPrimesUpTo(11));
   }
 }
--- prime-factors/PrimeFactorsApp-05/PrimeFactors/Sieve.cs
+++ prime-factors/PrimeFactorsApp-05/PrimeFactors/Sieve.cs
@@ -2,6 +2,14 @@
 
 public class Sieve {
   public IEnumerable<long> GetPrimesUpTo(long n) {
+    if (n < 2) {
+      return Enumerable.Empty<long>();
+    }
+
+    return PrimesUpTo(n);
+  }
+
+  IEnumerable<long> PrimesUpTo(long n) {
     List<long> primes = new();
     bool[] composites = new bool[n + 1];
     for (long candidate = 2; candidate <= n; candidate++) {
