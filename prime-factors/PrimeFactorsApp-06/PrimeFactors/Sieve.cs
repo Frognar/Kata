c39fe7b70ea9414e1c949ebcd6b2c3e5d8b841ed
Frognar <sebastian.przyszlak@gmail.com>
2022-07-07 18:08:43 +0200

    p4

--- prime-factors/PrimeFactorsApp-06/PrimeFactors/Sieve.cs
+++ prime-factors/PrimeFactorsApp-06/PrimeFactors/Sieve.cs
@@ -7,8 +7,14 @@ public class Sieve {
     }
 
     List<long> primes = new();
+    bool[] composites = new bool[n + 1];
     for (long candidate = 2; candidate <= n; candidate++) {
-      primes.Add(candidate);
+      if (composites[candidate] == false) {
+        primes.Add(candidate);
+        for (long multiply = 2 * candidate; multiply <= n; multiply += candidate) {
+          composites[multiply] = true;
+        }
+      }
     }
 
     return primes;
