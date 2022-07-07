207ef07716e60c9a76f1e9357665352022f89a48
Frognar <sebastian.przyszlak@gmail.com>
2022-07-07 18:13:22 +0200

    2^31 -1

--- prime-factors/PrimeFactorsApp-06/PrimeFactors.Tests/PrimeFactorsTest.cs
+++ prime-factors/PrimeFactorsApp-06/PrimeFactors.Tests/PrimeFactorsTest.cs
@@ -15,5 +15,6 @@ public class PrimeFactorsTest {
     Assert.Equal(new List<long> { 3, 3 }, primeFactors.Of(9));
     Assert.Equal(new List<long> { 2, 2, 3, 5, 7, 11 }, primeFactors.Of(2 * 2 * 3 * 5 * 7 * 11));
     Assert.Equal(new List<long> { 2, 2, 3, 5, 7, 11, 11, 13 }, primeFactors.Of(2 * 2 * 3 * 5 * 7 * 11 * 11 * 13));
+    Assert.Equal(new List<long> { (long)Math.Pow(2, 31) - 1 }, primeFactors.Of((long)Math.Pow(2, 31) - 1));
   }
 }
