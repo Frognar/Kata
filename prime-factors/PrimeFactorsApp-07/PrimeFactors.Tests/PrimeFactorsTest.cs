2af7f78acc4884cf7e8617eebad0e714163cdc12
Frognar <sebastian.przyszlak@gmail.com>
2022-07-08 18:49:23 +0200

    f8

--- prime-factors/PrimeFactorsApp-07/PrimeFactors.Tests/PrimeFactorsTest.cs
+++ prime-factors/PrimeFactorsApp-07/PrimeFactors.Tests/PrimeFactorsTest.cs
@@ -7,5 +7,9 @@ public class PrimeFactorsTest {
     Assert.Equal(new List<long> { 2 }, factors.Of(2));
     Assert.Equal(new List<long> { 3 }, factors.Of(3));
     Assert.Equal(new List<long> { 2, 2 }, factors.Of(4));
+    Assert.Equal(new List<long> { 5 }, factors.Of(5));
+    Assert.Equal(new List<long> { 2, 3 }, factors.Of(6));
+    Assert.Equal(new List<long> { 7 }, factors.Of(7));
+    Assert.Equal(new List<long> { 2, 2, 2 }, factors.Of(8));
   }
 }
