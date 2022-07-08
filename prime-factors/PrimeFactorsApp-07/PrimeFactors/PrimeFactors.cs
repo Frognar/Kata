5ba9959b1f392cbd0b0726b5e23d3b14bad59cc4
Frognar <sebastian.przyszlak@gmail.com>
2022-07-08 18:51:35 +0200

    sieve class

--- prime-factors/PrimeFactorsApp-07/PrimeFactors/PrimeFactors.cs
+++ prime-factors/PrimeFactorsApp-07/PrimeFactors/PrimeFactors.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-
-namespace PrimeFactors;
+﻿namespace PrimeFactors;
 public class PrimeFactors {
   public IEnumerable<long> Of(long n) {
     List<long> factors = new();
