d0dc0b3366a57ce69114863d8a7eb2c3c111acfb
Frognar <sebastian.przyszlak@gmail.com>
2022-07-09 14:16:50 +0200

    primes_up_to

--- prime-factors/prime_factors_pyapp_01/prime_factors.py
+++ prime-factors/prime_factors_pyapp_01/prime_factors.py
@@ -1,8 +1,12 @@
 def factors_of(n):
     factors = []
-    for prime in range(2, n + 1):
+    for prime in primes_up_to(n):
         while n % prime == 0:
             factors.append(prime)
             n /= prime
 
     return factors
+
+
+def primes_up_to(n):
+    return range(2, n + 1)
