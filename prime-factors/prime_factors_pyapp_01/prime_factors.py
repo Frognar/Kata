14753185354ebba03f6916a832d6158f0477315f
Frognar <sebastian.przyszlak@gmail.com>
2022-07-09 14:14:39 +0200

    refactor

--- prime-factors/prime_factors_pyapp_01/prime_factors.py
+++ prime-factors/prime_factors_pyapp_01/prime_factors.py
@@ -1,11 +1,8 @@
 def factors_of(n):
     factors = []
-    if n > 1:
-        for prime in range(2, n + 1):
-            while n % prime == 0:
-                factors.append(prime)
-                n /= prime
+    for prime in range(2, n + 1):
+        while n % prime == 0:
+            factors.append(prime)
+            n /= prime
 
-    if n > 1:
-        factors.append(n)
     return factors
