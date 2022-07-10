bbbba4a6babfe9e2f7dbc792868bb8be2e50c431
Frognar <sebastian.przyszlak@gmail.com>
2022-07-10 10:37:52 +0200

    p4

--- prime-factors/prime_factors_pyapp_02/factors_of_tests.py
+++ prime-factors/prime_factors_pyapp_02/factors_of_tests.py
@@ -16,10 +16,13 @@ def factors_of(n):
 
 
 def primes_up_to(limit):
+    composites = [False for _ in range(limit + 1)]
     primes = []
     for candidate in range(2, limit + 1):
-        primes.append(candidate)
-
+        if not composites[candidate]:
+            for multiply in range(2 * candidate, limit + 1, candidate):
+                composites[multiply] = True
+            primes.append(candidate)
     return primes
 
 
@@ -39,6 +42,7 @@ class FactorsOfTestCase(unittest.TestCase):
         self.assertEqual([], primes_up_to(1))
         self.assertEqual([2], primes_up_to(2))
         self.assertEqual([2, 3], primes_up_to(3))
+        self.assertEqual([2, 3], primes_up_to(4))
 
 
 if __name__ == '__main__':
