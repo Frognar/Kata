daba48d3e3cd934efbef72cb9cf3569cc2756535
Frognar <sebastian.przyszlak@gmail.com>
2022-07-10 10:30:20 +0200

    p1

--- prime-factors/prime_factors_pyapp_02/factors_of_tests.py
+++ prime-factors/prime_factors_pyapp_02/factors_of_tests.py
@@ -15,6 +15,10 @@ def factors_of(n):
     return factors
 
 
+def primes_up_to(limit):
+    return []
+
+
 class FactorsOfTestCase(unittest.TestCase):
     def test_factors_of(self):
         self.assertEqual([], factors_of(1))
@@ -25,6 +29,10 @@ class FactorsOfTestCase(unittest.TestCase):
         self.assertEqual([2, 3], factors_of(6))
         self.assertEqual([7], factors_of(7))
         self.assertEqual([2, 2, 2], factors_of(8))
+        #self.assertEqual([3, 3], factors_of(9))
+
+    def test_primes_up_to(self):
+        self.assertEqual([], primes_up_to(1))
 
 
 if __name__ == '__main__':
