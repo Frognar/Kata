e187c9c3bdba5366055f5c00c9465c70bd2838c3
Frognar <sebastian.przyszlak@gmail.com>
2022-07-11 13:12:32 +0200

    move functions to other file

--- prime-factors/prime_factors_pyapp_03/factors_of_tests.py
+++ prime-factors/prime_factors_pyapp_03/factors_of_tests.py
@@ -1,28 +1,6 @@
-import math
 import unittest
 
-
-def factors_of(n):
-    factors = []
-
-    limit = int(math.sqrt(n))
-    for prime in primes_up_to(limit):
-        while n % prime == 0:
-            factors.append(prime)
-            n //= prime
-
-    if n > 1:
-        factors.append(n)
-    return factors
-
-
-def primes_up_to(limit):
-    primes = [True for _ in range(limit + 1)]
-    for candidate in range(2, limit + 1):
-        if primes[candidate]:
-            for multiply in range(2 * candidate, limit + 1, candidate):
-                primes[multiply] = False
-            yield candidate
+from prime_factors import factors_of, primes_up_to
 
 
 class FactorsOfTestCase(unittest.TestCase):
---
+++ prime-factors/prime_factors_pyapp_03/prime_factors.py
@@ -0,0 +1,25 @@
+import math
+
+
+def factors_of(n):
+    factors = []
+
+    limit = int(math.sqrt(n))
+    for prime in primes_up_to(limit):
+        while n % prime == 0:
+            factors.append(prime)
+            n //= prime
+
+    if n > 1:
+        factors.append(n)
+
+    return factors
+
+
+def primes_up_to(limit):
+    primes = [True for _ in range(limit + 1)]
+    for candidate in range(2, limit + 1):
+        if primes[candidate]:
+            for multiply in range(2 * candidate, limit + 1, candidate):
+                primes[multiply] = False
+            yield candidate
