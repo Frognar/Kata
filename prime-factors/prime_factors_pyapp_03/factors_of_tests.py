6cf90c0170a3e6539cca15ef9c42f9ae18d473cb
Frognar <sebastian.przyszlak@gmail.com>
2022-07-11 12:57:16 +0200

    f3

--- prime-factors/prime_factors_pyapp_03/factors_of_tests.py
+++ prime-factors/prime_factors_pyapp_03/factors_of_tests.py
@@ -4,7 +4,7 @@ import unittest
 def factors_of(n):
     factors = []
     if n > 1:
-        factors.append(2)
+        factors.append(n)
     return factors
 
 
@@ -12,6 +12,7 @@ class FactorsOfTestCase(unittest.TestCase):
     def test_factors_of(self):
         self.assertEqual([], factors_of(1))
         self.assertEqual([2], factors_of(2))
+        self.assertEqual([3], factors_of(3))
 
 
 if __name__ == '__main__':
