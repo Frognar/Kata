50fe348c1155e44a832790785b2224e02cdeb259
Frognar <sebastian.przyszlak@gmail.com>
2022-07-12 08:58:50 +0200

    f8

--- prime-factors/prime_factors_pyapp_04/prime_factors_tests.py
+++ prime-factors/prime_factors_pyapp_04/prime_factors_tests.py
@@ -4,7 +4,7 @@ import unittest
 def factors_of(n):
     factors = []
     if n > 1:
-        if n % 2 == 0:
+        while n % 2 == 0:
             factors.append(2)
             n //= 2
 
@@ -20,6 +20,10 @@ class PrimeFactorsTestCase(unittest.TestCase):
         self.assertEqual([2], factors_of(2))
         self.assertEqual([3], factors_of(3))
         self.assertEqual([2, 2], factors_of(4))
+        self.assertEqual([5], factors_of(5))
+        self.assertEqual([2, 3], factors_of(6))
+        self.assertEqual([7], factors_of(7))
+        self.assertEqual([2, 2, 2], factors_of(8))
 
 
 if __name__ == '__main__':
