9d41a4ff607a0cff09eceabba3651e603a677974
Frognar <sebastian.przyszlak@gmail.com>
2022-07-12 09:08:44 +0200

    move methods to other file

---
+++ prime-factors/prime_factors_pyapp_04/prime_factors.py
@@ -0,0 +1,23 @@
+import math
+
+
+def factors_of(n):
+    factors = []
+    for prime in primes_up_to(int(math.sqrt(n))):
+        while n % prime == 0:
+            factors.append(prime)
+            n //= prime
+
+    if n > 1:
+        factors.append(n)
+
+    return factors
+
+
+def primes_up_to(limit):
+    primes = [True for _ in range(limit + 1)]
+    for candidate in range(2, limit + 1):
+        if primes[candidate]:
+            for multiply in range(2 * candidate, limit + 1, candidate):
+                primes[multiply] = False
+            yield candidate
--- prime-factors/prime_factors_pyapp_04/prime_factors_tests.py
+++ prime-factors/prime_factors_pyapp_04/prime_factors_tests.py
@@ -1,27 +1,7 @@
 import math
 import unittest
 
-
-def factors_of(n):
-    factors = []
-    for prime in primes_up_to(int(math.sqrt(n))):
-        while n % prime == 0:
-            factors.append(prime)
-            n //= prime
-
-    if n > 1:
-        factors.append(n)
-
-    return factors
-
-
-def primes_up_to(limit):
-    primes = [True for _ in range(limit + 1)]
-    for candidate in range(2, limit + 1):
-        if primes[candidate]:
-            for multiply in range(2 * candidate, limit + 1, candidate):
-                primes[multiply] = False
-            yield candidate
+from prime_factors import factors_of, primes_up_to
 
 
 class PrimeFactorsTestCase(unittest.TestCase):
