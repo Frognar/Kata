a286b765fe17a3b4d0ed02f6ec0bd1fbe6527338
Frognar <sebastian.przyszlak@gmail.com>
2022-07-13 13:52:20 +0200

    f8 use primes_up_to function

--- prime-factors/prime_factors_pyapp_05/prime_factors.py
+++ prime-factors/prime_factors_pyapp_05/prime_factors.py
@@ -1,7 +1,6 @@
 def factors_of(n):
     factors = []
-    primes = [2]
-    for prime in primes:
+    for prime in primes_up_to(2):
         while n % prime == 0:
             factors.append(prime)
             n //= prime
