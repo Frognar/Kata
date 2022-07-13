3b483263954a34658a1c698d86a3e50b90c1908a
Frognar <sebastian.przyszlak@gmail.com>
2022-07-13 13:51:16 +0200

    p4

--- prime-factors/prime_factors_pyapp_05/prime_factors.py
+++ prime-factors/prime_factors_pyapp_05/prime_factors.py
@@ -13,5 +13,10 @@ def factors_of(n):
 
 
 def primes_up_to(limit):
+    primes = [True for _ in range(limit + 1)]
     for candidate in range(2, limit + 1):
-        yield candidate
+        if primes[candidate]:
+            for multiply in range(2 * candidate, limit + 1, candidate):
+                primes[multiply] = False
+
+            yield candidate
