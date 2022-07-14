3ee729202b18488786f29d74b1bfd21c5645466d
Frognar <sebastian.przyszlak@gmail.com>
2022-07-14 09:39:46 +0200

    f1

--- prime-factors/prime_factors_pyapp_06/prime_factors_tests.py
+++ prime-factors/prime_factors_pyapp_06/prime_factors_tests.py
@@ -5,7 +5,7 @@ from prime_factors import *
 
 class PrimeFactorsTestCase(unittest.TestCase):
     def test_factors_of(self):
-        pass
+        self.assertEqual([], factors_of(1))
 
 
 if __name__ == '__main__':
