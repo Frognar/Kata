873b866390ebcc74c76f9b6b04ea7186730671d9
Frognar <sebastian.przyszlak@gmail.com>
2022-07-16 10:38:24 +0200

    f8

--- prime-factors/prime_factors_cjapp_01/spec/prime_factors_cjapp_01/core_spec.clj
+++ prime-factors/prime_factors_cjapp_01/spec/prime_factors_cjapp_01/core_spec.clj
@@ -4,12 +4,22 @@
 
 
 (describe "factors of"
-          (it "should return prime factors of n"
-              (should= [] (factors-of 1))
-              (should= [2] (factors-of 2))
-              (should= [3] (factors-of 3))
-              (should= [2 2] (factors-of 4))
-              )
+          (it "1 -> []"
+              (should= [] (factors-of 1)))
+          (it "2 -> [2]"
+              (should= [2] (factors-of 2)))
+          (it "3 -> [3]"
+              (should= [3] (factors-of 3)))
+          (it "4 -> [2 2]"
+              (should= [2 2] (factors-of 4)))
+          (it "5 -> [5]"
+              (should= [5] (factors-of 5)))
+          (it "6 -> [2 3]"
+              (should= [2 3] (factors-of 6)))
+          (it "7 -> [7]"
+              (should= [7] (factors-of 7)))
+          (it "8 -> [2 2 2]"
+              (should= [2 2 2] (factors-of 8)))
           )
 
 (run-specs)
