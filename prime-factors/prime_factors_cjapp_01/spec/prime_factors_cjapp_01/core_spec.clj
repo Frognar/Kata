c8f08d0530cffb74481c876322a5c731a62732d2
Frognar <sebastian.przyszlak@gmail.com>
2022-07-16 10:18:19 +0200

    f1

--- prime-factors/prime_factors_cjapp_01/spec/prime_factors_cjapp_01/core_spec.clj
+++ prime-factors/prime_factors_cjapp_01/spec/prime_factors_cjapp_01/core_spec.clj
@@ -3,4 +3,10 @@
             [prime-factors-cjapp-01.core :refer :all]))
 
 
+(describe "factors of"
+          (it "should be [] for 1"
+              (should= [] (factors-of 1))
+              )
+          )
+
 (run-specs)
--- prime-factors/prime_factors_cjapp_01/src/prime_factors_cjapp_01/core.clj
+++ prime-factors/prime_factors_cjapp_01/src/prime_factors_cjapp_01/core.clj
@@ -1,1 +1,6 @@
 (ns prime-factors-cjapp-01.core)
+
+
+(defn factors-of [n]
+  []
+  )
