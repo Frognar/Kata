10504fbe0b1a26e5f2ca2bdbad950fb7f34cd23b
Frognar <sebastian.przyszlak@gmail.com>
2022-07-17 09:39:15 +0200

    2^31-1 -> [2^31-1]

--- prime-factors/prime_factors_cjapp_02/spec/prime_factors_cjapp_02/core_spec.clj
+++ prime-factors/prime_factors_cjapp_02/spec/prime_factors_cjapp_02/core_spec.clj
@@ -23,6 +23,9 @@
               (should= [3 3] (factors-of 9)))
           (it "2*2*3*3*5*5*7*7*11*11 -> [2 2 3 3 5 5 7 7 11 11]"
               (should= [2 2 3 3 5 5 7 7 11 11] (factors-of (* 2 2 3 3 5 5 7 7 11 11))))
+          (it "2^31-1 -> [2^31-1]"
+              (let [m31 (dec (apply * (repeat 31 2)))]
+                 (should= [m31] (factors-of m31))))
           )
 
 (run-specs)
--- prime-factors/prime_factors_cjapp_02/src/prime_factors_cjapp_02/core.clj
+++ prime-factors/prime_factors_cjapp_02/src/prime_factors_cjapp_02/core.clj
@@ -3,11 +3,11 @@
 (defn factors-of [n]
   (loop [div 2 n n factors []]
     (if (> n 1)
-      (if (zero? (mod n div))
-        (recur div
-               (/ n div)
-               (conj factors div))
-        (recur (inc div)
-               n
-               factors))
+      (cond
+        (> div (Math/sqrt n))
+        (conj factors n)
+        (zero? (mod n div))
+        (recur div (/ n div) (conj factors div))
+        :else
+        (recur (inc div) n factors))
       factors)))
