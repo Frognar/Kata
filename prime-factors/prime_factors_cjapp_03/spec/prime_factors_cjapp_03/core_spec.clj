72bc1fd325d1fa68ef10ebc940566b76d87fe1e0
Frognar <sebastian.przyszlak@gmail.com>
2022-07-18 12:41:38 +0200

    Init project

---
+++ prime-factors/prime_factors_cjapp_03/spec/prime_factors_cjapp_03/core_spec.clj
@@ -0,0 +1,9 @@
+(ns prime-factors-cjapp-03.core-spec
+  (:require [speclj.core :refer :all]
+            [prime-factors-cjapp-03.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
+
+(run-specs)
---
+++ prime-factors/prime_factors_cjapp_03/src/prime_factors_cjapp_03/core.clj
@@ -0,0 +1,5 @@
+(ns prime-factors-cjapp-03.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
