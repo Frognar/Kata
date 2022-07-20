c0046d143a6bf66df48678ef854e323b53c7470f
Frognar <sebastian.przyszlak@gmail.com>
2022-07-20 09:27:56 +0200

    (* 2 3 5 7 11 13 17) -> [2 3 5 7 11 13 17]

--- prime-factors/prime_factors_cjapp_05/spec/prime_factors_cjapp_05/core_spec.clj
+++ prime-factors/prime_factors_cjapp_05/spec/prime_factors_cjapp_05/core_spec.clj
@@ -21,6 +21,8 @@
               (should= [2 2 2] (factors-of 8)))
           (it "9 -> [3 3]"
               (should= [3 3] (factors-of 9)))
+          (it "(* 2 3 5 7 11 13 17) -> [2 3 5 7 11 13 17]"
+              (should= [2 3 5 7 11 13 17] (factors-of (* 2 3 5 7 11 13 17))))
           )
 
 (run-specs)
