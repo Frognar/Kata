5b1d58204027cff395024f572cd896fc73c4722e
Frognar <sebastian.przyszlak@gmail.com>
2022-07-20 09:25:16 +0200

    9 -> [3 3]

--- prime-factors/prime_factors_cjapp_05/src/prime_factors_cjapp_05/core.clj
+++ prime-factors/prime_factors_cjapp_05/src/prime_factors_cjapp_05/core.clj
@@ -3,7 +3,11 @@
 
 (defn factors-of [n]
   (if (> n 1)
-    (if (zero? (mod n 2))
+    (cond
+      (zero? (mod n 2))
       (concat [2] (factors-of (/ n 2)))
+      (zero? (mod n 3))
+      (concat [3] (factors-of (/ n 3)))
+      :else
       [n])
     []))
