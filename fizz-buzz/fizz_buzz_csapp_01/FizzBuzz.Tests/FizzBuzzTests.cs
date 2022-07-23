2462da797006d1ddc36099395b7f237d51a56ed3
Frognar <sebastian.przyszlak@gmail.com>
2022-07-23 10:16:12 +0200

    Fizz Buzz 1

--- fizz-buzz/fizz_buzz_csapp_01/FizzBuzz.Tests/FizzBuzzTests.cs
+++ fizz-buzz/fizz_buzz_csapp_01/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -3,5 +3,7 @@ namespace FizzBuzz.Tests;
 public class FizzBuzzTests {
   [Fact]
   public void FizzBuzz_Tests() {
+    FizzBuzz fizzBuzz = new();
+    Assert.Equal("1", fizzBuzz.Render(1));
   }
 }
