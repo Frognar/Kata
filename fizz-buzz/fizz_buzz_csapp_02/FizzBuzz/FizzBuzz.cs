b4aaaa005f295985b4623373f6f98c22e2ea8890
Frognar <sebastian.przyszlak@gmail.com>
2022-07-24 16:45:50 +0200

    fb3

--- fizz-buzz/fizz_buzz_csapp_02/FizzBuzz/FizzBuzz.cs
+++ fizz-buzz/fizz_buzz_csapp_02/FizzBuzz/FizzBuzz.cs
@@ -1,6 +1,16 @@
 ﻿namespace FizzBuzz;
 
 public class FizzBuzz {
-  public string Render(int number)
-    => number.ToString();
+  public string Render(int number) {
+    string result = string.Empty;
+    if (number == 3) {
+      result = "Fizz";
+    }
+
+    if (string.IsNullOrEmpty(result)) {
+      result = number.ToString();
+    }
+
+    return result;
+  }
 }
