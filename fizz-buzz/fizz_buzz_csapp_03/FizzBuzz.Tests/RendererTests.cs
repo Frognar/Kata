55f572047da4b9318b99700e4858662aabb7fa83
Frognar <sebastian.przyszlak@gmail.com>
2022-07-25 19:22:16 +0200

    show all numbers

---
+++ fizz-buzz/fizz_buzz_csapp_03/FizzBuzz.Tests/RendererTests.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuzz.Tests;
+
+public class RendererTests {
+  int displayedCount = 0;
+
+  [Fact]
+  public void ShowNumbers_ShouldShowAllNumbers() {
+    Renderer renderer = new(TestDisplay);
+    renderer.ShowNumbers(2);
+    Assert.Equal(2, displayedCount);
+  }
+
+  void TestDisplay(string text) {
+    displayedCount++;
+  }
+}
