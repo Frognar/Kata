6749ce2e5db3396a6a270879a7c5e857edc28978
Frognar <sebastian.przyszlak@gmail.com>
2022-07-26 21:16:14 +0200

    Show all numbers

---
+++ fizz-buzz/fizz_buzz_csapp_04/FizzBuzz/Renderer.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzz;
+
+public class Renderer {
+  readonly Action<string> display;
+
+  public Renderer(Action<string> display) {
+    this.display = display;
+  }
+
+  public void ShowNumbers(int count) {
+    for (int i = 0; i < count; i++) {
+      display("txt");
+    }
+  }
+}
