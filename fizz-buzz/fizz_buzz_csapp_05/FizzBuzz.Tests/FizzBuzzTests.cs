ecff92630289936bc3cf188063556386bcc272bf
Frognar <sebastian.przyszlak@gmail.com>
2022-07-27 20:19:02 +0200

    fb2

--- fizz-buzz/fizz_buzz_csapp_05/FizzBuzz.Tests/FizzBuzzTests.cs
+++ fizz-buzz/fizz_buzz_csapp_05/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -3,8 +3,7 @@ namespace FizzBuzz.Tests;
 public class FizzBuzzTests {
   [Fact]
   public void FizzBuzz_Test() {
-    FizzBuzz fizzBuzz = new();
-    Assert.Equal("1", fizzBuzz.Render(1));
-    Assert.Equal("2", fizzBuzz.Render(2));
+    Assert.Equal("1", FizzBuzz.Render(1));
+    Assert.Equal("2", FizzBuzz.Render(2));
   }
 }
