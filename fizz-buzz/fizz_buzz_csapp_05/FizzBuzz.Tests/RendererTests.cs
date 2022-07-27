1db432a35054a5f6b74e3d4557d6c6907c26a100
Frognar <sebastian.przyszlak@gmail.com>
2022-07-27 20:27:15 +0200

    render number

--- fizz-buzz/fizz_buzz_csapp_05/FizzBuzz.Tests/RendererTests.cs
+++ fizz-buzz/fizz_buzz_csapp_05/FizzBuzz.Tests/RendererTests.cs
@@ -2,14 +2,26 @@
 
 public class RendererTests {
   int displayedCount = 0;
+  readonly List<string> displayed = new();
+
   [Fact]
   public void ShowNumbers_ShouldDisplayAllNumbers() {
     Renderer renderer = new(TestDisplay);
-    renderer.ShowNumbers(2);
+    renderer.ShowNumbers(2, TestRender);
     Assert.Equal(2, displayedCount);
+  }
+
+  [Fact]
+  public void ShowNumbers_ShouldRenderNumber() {
+    Renderer renderer = new(TestDisplay);
+    renderer.ShowNumbers(1, TestRender);
+    Assert.Equal("x1", displayed[0]);
   }
 
   void TestDisplay(string text) {
     displayedCount++;
+    displayed.Add(text);
   }
+
+  string TestRender(int number) => $"x{number}";
 }
