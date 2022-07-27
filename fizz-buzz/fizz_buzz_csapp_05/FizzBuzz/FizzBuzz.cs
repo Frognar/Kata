23e36b3243bcc2c644b5c8ae75e19dc38074480a
Frognar <sebastian.przyszlak@gmail.com>
2022-07-27 20:21:37 +0200

    fb6

--- fizz-buzz/fizz_buzz_csapp_05/FizzBuzz/FizzBuzz.cs
+++ fizz-buzz/fizz_buzz_csapp_05/FizzBuzz/FizzBuzz.cs
@@ -4,7 +4,7 @@ public class FizzBuzz {
   public static string Render(int number) {
     string? result = null;
 
-    if (number == 3) {
+    if (number % 3 == 0) {
       result = "Fizz";
     }
 
