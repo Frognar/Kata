b11eb72f43c9aabf6520c6b0fc7f8713923ec09b
Frognar <sebastian.przyszlak@gmail.com>
2022-07-29 21:23:40 +0200

    FizzBuzz Final

--- fizz-buzz/fizz_buzz_csapp_07/FizzBuzz.Tests/RendererTests.cs
+++ fizz-buzz/fizz_buzz_csapp_07/FizzBuzz.Tests/RendererTests.cs
@@ -18,6 +18,19 @@ public class RendererTests {
     Assert.Equal("_1", displayed[0]);
   }
 
+  [Fact]
+  public void FizzBuzz_FinalTest() {
+    Renderer renderer = new(Display);
+    renderer.ShowNumbers(100, FizzBuzz.Render);
+    Assert.Equal("Fizz", displayed[2]);
+    Assert.Equal("Buzz", displayed[4]);
+    Assert.Equal("FizzBuzz", displayed[14]);
+    Assert.Equal("16", displayed[15]);
+    Assert.Equal("FizzBuzz", displayed[44]);
+    Assert.Equal("Buzz", displayed[49]);
+    Assert.Equal("Fizz", displayed[98]);
+  }
+
   void Display(string text) {
     displayedCount++;
     displayed.Add(text);
