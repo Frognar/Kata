7ff468a2910b28fa3ece24ed6eaffc228ee54837
Frognar <sebastian.przyszlak@gmail.com>
2022-07-30 08:49:01 +0200

    1

--- fizz-buzz/fizz_buzz_pyapp_01/FizzBuzzTests.py
+++ fizz-buzz/fizz_buzz_pyapp_01/FizzBuzzTests.py
@@ -1,9 +1,12 @@
 import unittest
 
+from FizzBuzz import FizzBuzz
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_FizzBuzz(self):
+        fizz_buzz = FizzBuzz()
+        self.assertEqual("1", fizz_buzz.render(1))  # add assertion here
 
 
 if __name__ == '__main__':
