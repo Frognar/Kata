f132d1461bfa2338e0ae1b1fde57a3fd17ebf283
Frognar <sebastian.przyszlak@gmail.com>
2022-07-30 08:52:08 +0200

    4

--- fizz-buzz/fizz_buzz_pyapp_01/FizzBuzzTests.py
+++ fizz-buzz/fizz_buzz_pyapp_01/FizzBuzzTests.py
@@ -9,6 +9,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual("1", fizz_buzz.render(1))
         self.assertEqual("2", fizz_buzz.render(2))
         self.assertEqual("Fizz", fizz_buzz.render(3))
+        self.assertEqual("4", fizz_buzz.render(4))
 
 
 if __name__ == '__main__':
