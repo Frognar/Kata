f9f53a80097548f8cbdfc29d641d4ea0d203c45d
Frognar <sebastian.przyszlak@gmail.com>
2022-07-30 09:06:16 +0200

    FizzBuzz final test

--- fizz-buzz/fizz_buzz_pyapp_01/Renderer.py
+++ fizz-buzz/fizz_buzz_pyapp_01/Renderer.py
@@ -2,5 +2,5 @@ class Renderer:
     def __init__(self, display):
         self.display = display
 
-    def show_numbers(self, count):
-        [self.display("txt") for _ in range(count)]
+    def show_numbers(self, count, render):
+        [self.display(render(i + 1)) for i in range(count)]
--- fizz-buzz/fizz_buzz_pyapp_01/RendererTests.py
+++ fizz-buzz/fizz_buzz_pyapp_01/RendererTests.py
@@ -1,19 +1,35 @@
 import unittest
 
+from FizzBuzz import fizz_buzz
 from Renderer import Renderer
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.displayedCount = 0
+        self.displayed = []
         self.renderer = Renderer(self.display)
 
     def test_show_numbers_should_display_each_number(self):
-        self.renderer.show_numbers(2)
+        self.renderer.show_numbers(2, self.render)
         self.assertEqual(2, self.displayedCount)
+
+    def test_show_numbers_should_render_number(self):
+        self.renderer.show_numbers(1, self.render)
+        self.assertEqual("s1", self.displayed[0])
+
+    @staticmethod
+    def test_fizz_buzz_test():
+        renderer = Renderer(print)
+        renderer.show_numbers(100, fizz_buzz)
 
     def display(self, text):
         self.displayedCount += 1
+        self.displayed.append(text)
+
+    @staticmethod
+    def render(number):
+        return f's{number}'
 
 
 if __name__ == '__main__':
