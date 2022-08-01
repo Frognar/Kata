4ad71962e3bc26259b4455054b31aeacf8f2e131
Frognar <sebastian.przyszlak@gmail.com>
2022-08-01 21:50:45 +0200

    fb1

---
+++ fizz-buzz/fizz_buzz_pyapp_03/FizzBuzzTests.py
@@ -0,0 +1,12 @@
+import unittest
+
+from FizzBuzz import fizz_buzz
+
+
+class FizzBuzzTestCase(unittest.TestCase):
+    def test_fizz_buzz(self):
+        self.assertEqual('1', fizz_buzz(1))
+
+
+if __name__ == '__main__':
+    unittest.main()
