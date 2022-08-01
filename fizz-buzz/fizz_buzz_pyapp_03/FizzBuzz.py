a62b110a381d0605f25867871aa45dc9c551df86
Frognar <sebastian.przyszlak@gmail.com>
2022-08-01 21:53:15 +0200

    fb10

--- fizz-buzz/fizz_buzz_pyapp_03/FizzBuzz.py
+++ fizz-buzz/fizz_buzz_pyapp_03/FizzBuzz.py
@@ -2,6 +2,6 @@ def fizz_buzz(number):
     result = str(number)
     if number % 3 == 0:
         result = 'Fizz'
-    if number == 5:
+    if number % 5 == 0:
         result = 'Buzz'
     return result
