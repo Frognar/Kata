a1bb0adf8e45dbbcefd5583c9a8a0409d763e411
Frognar <sebastian.przyszlak@gmail.com>
2022-08-01 21:47:25 +0200

    display each number

---
+++ fizz-buzz/fizz_buzz_pyapp_03/Renderer.py
@@ -0,0 +1,8 @@
+class Renderer:
+    def __init__(self, display):
+        self.display = display
+
+    def show_numbers(self, count):
+        [self.display('') for _ in range(count)]
+
+    
--- fizz-buzz/fizz_buzz_pyapp_03/RendererTests.py
+++ fizz-buzz/fizz_buzz_pyapp_03/RendererTests.py
@@ -1,9 +1,19 @@
 import unittest
 
+from Renderer import Renderer
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class RendererTestCase(unittest.TestCase):
+    def setUp(self):
+        self.displayedCount = 0
+        self.renderer = Renderer(self.display)
+
+    def test_renderer_show_numbers_should_display_each_number(self):
+        self.renderer.show_numbers(3)
+        self.assertEqual(3, self.displayedCount)
+
+    def display(self, text):
+        self.displayedCount += 1
 
 
 if __name__ == '__main__':
