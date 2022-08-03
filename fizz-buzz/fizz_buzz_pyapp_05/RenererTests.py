706caec84d657f7859fdf2bd6a5000dd39c56c79
Frognar <sebastian.przyszlak@gmail.com>
2022-08-03 21:07:54 +0200

    display each

---
+++ fizz-buzz/fizz_buzz_pyapp_05/RenererTests.py
@@ -0,0 +1,20 @@
+import unittest
+
+from Renderer import Renderer
+
+
+class RendererTestCase(unittest.TestCase):
+    def setUp(self):
+        self.displayedCount = 0
+        self.renderer = Renderer(self.display)
+
+    def display(self, text):
+        self.displayedCount += 1
+
+    def test_show_numbers_should_display_each_number(self):
+        self.renderer.show_numbers(2)
+        self.assertEqual(2, self.displayedCount)
+
+
+if __name__ == '__main__':
+    unittest.main()
