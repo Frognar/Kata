57c45f7e3e1ab88ab1b5cd76d379416a119c90b6
Frognar <sebastian.przyszlak@gmail.com>
2022-08-04 14:38:51 +0200

    render number

--- fizz-buzz/fizz_buzz_pyapp_06/RendererTests.py
+++ fizz-buzz/fizz_buzz_pyapp_06/RendererTests.py
@@ -1,19 +1,30 @@
 import unittest
 
+from FizzBuzz import fizz_buzz
 from Renderer import Renderer
 
 
 class RendererTestCase(unittest.TestCase):
     def setUp(self):
         self.displayedCount = 0
+        self.displayed = []
         self.renderer = Renderer(self.display)
 
     def display(self, text):
         self.displayedCount += 1
+        self.displayed.append(text)
+
+    @staticmethod
+    def render(number):
+        return f'_x{number}x_'
 
     def test_show_numbers_should_display_each_number(self):
-        self.renderer.show_numbers(2)
+        self.renderer.show_numbers(2, self.render)
         self.assertEqual(2, self.displayedCount)
+
+    def test_show_numbers_should_render_number(self):
+        self.renderer.show_numbers(1, self.render)
+        self.assertEqual('_x1x_', self.displayed[0])
 
 
 if __name__ == '__main__':
