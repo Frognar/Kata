7cf4baf361483e080b9888d47ee34e302d826304
Frognar <sebastian.przyszlak@gmail.com>
2022-08-10 14:43:25 +0200

    fb10

--- fizz-buzz/fizz_buzz_cljapp_05/src/fizz_buzz_cljapp_05/core.clj
+++ fizz-buzz/fizz_buzz_cljapp_05/src/fizz_buzz_cljapp_05/core.clj
@@ -3,10 +3,13 @@
 (defn fizz? [n]
   (zero? (mod n 3)))
 
+(defn buzz? [n]
+  (zero? (mod n 5)))
+
 (defn fizz-buzz [n]
   (cond
     (fizz? n) "Fizz"
-    (zero? (mod n 5)) "Buzz"
+    (buzz? n) "Buzz"
     :else (str n)
     )
   )
