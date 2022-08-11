bb4e50046add2a39850d59e1fc4eb3a15e09ca73
Frognar <sebastian.przyszlak@gmail.com>
2022-08-11 15:10:19 +0200

    Init project

---
+++ fizz-buzz/fizz_buzz_cljapp_06/spec/fizz_buzz_cljapp_06/core_spec.clj
@@ -0,0 +1,9 @@
+(ns fizz-buzz-cljapp-06.core-spec
+  (:require [speclj.core :refer :all]
+            [fizz-buzz-cljapp-06.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
+
+(run-specs)
---
+++ fizz-buzz/fizz_buzz_cljapp_06/src/fizz_buzz_cljapp_06/core.clj
@@ -0,0 +1,5 @@
+(ns fizz-buzz-cljapp-06.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
