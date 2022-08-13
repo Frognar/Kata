3ab87e086c7ff81a85f247329fc2935012de9417
Frognar <sebastian.przyszlak@gmail.com>
2022-08-13 11:38:29 +0200

    refactor

--- bowling-game/bowling-game-cs/BowlingGame.Tests/BowlingGameTests.cs
+++ bowling-game/bowling-game-cs/BowlingGame.Tests/BowlingGameTests.cs
@@ -9,23 +9,25 @@ public class BowlingGameTests
         game = new Game();
     }
 
+    private void RollMany(int n, int pins)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            game.Roll(pins);
+        }
+    }
+
     [Fact]
     public void RollAllZeros_ScoreShouldBeZero()
     {
-        for (int i = 0; i < 20; i++)
-        {
-            game.Roll(0);
-        }
+        RollMany(20, 0);
         Assert.Equal(0, game.Score());
     }
 
     [Fact]
     public void RollAllOnes_ScoreShouldBeTwenty()
     {
-        for (int i = 0; i < 20; i++)
-        {
-            game.Roll(1);
-        }
+        RollMany(20, 1);
         Assert.Equal(20, game.Score());
     }
 }
