d6446da19f88b5fa27e221c636bdb932bb092f19
Frognar <sebastian.przyszlak@gmail.com>
2022-08-14 10:28:18 +0200

    refactor

--- bowling-game/bowling-game-cs/BowlingGame.Tests/02/BowlingGameTests.cs
+++ bowling-game/bowling-game-cs/BowlingGame.Tests/02/BowlingGameTests.cs
@@ -3,14 +3,18 @@
 namespace BowlingGame.Tests._02; 
 
 public class BowlingGameTests {
+    readonly Game game;
+
+    public BowlingGameTests() {
+        game = new Game();
+    }
+
     [Fact]
     public void CanCreateGame() {
-        Game game = new();
     }
 
     [Fact]
     public void CanRoll() {
-        Game game = new();
         game.Roll(0);
     }
 }
