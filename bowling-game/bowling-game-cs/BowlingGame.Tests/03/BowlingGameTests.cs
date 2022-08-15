e7a4700ef6a4fad3774f6c876665c90386dc72bb
Frognar <sebastian.przyszlak@gmail.com>
2022-08-15 09:16:55 +0200

    create game

---
+++ bowling-game/bowling-game-cs/BowlingGame.Tests/03/BowlingGameTests.cs
@@ -0,0 +1,10 @@
+﻿using BowlingGame._03;
+
+namespace BowlingGame.Tests._03; 
+
+public class BowlingGameTests {
+  [Fact]
+  public void CanCreateGame() {
+    Game game = new();
+  }
+}
---
+++ bowling-game/bowling-game-cs/BowlingGame/03/Game.cs
@@ -0,0 +1,5 @@
+﻿namespace BowlingGame._03; 
+
+public class Game {
+  
+}
