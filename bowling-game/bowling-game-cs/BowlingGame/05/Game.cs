efe8ab4f6b4246a2b4997e66d7d7a388b1c7047c
Frognar <sebastian.przyszlak@gmail.com>
2022-08-17 19:27:34 +0200

    refactor

--- bowling-game/bowling-game-cs/BowlingGame/05/Game.cs
+++ bowling-game/bowling-game-cs/BowlingGame/05/Game.cs
@@ -12,7 +12,7 @@ public class Game {
     int score = 0;
     int firstInFrame = 0;
     for (int frame = 0; frame < 10; frame++) {
-      if (rolls[firstInFrame] == 10) {
+      if (IsStrike(firstInFrame)) {
         score += 10 + rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
         firstInFrame++;
       }
@@ -27,6 +27,8 @@ public class Game {
     }
     return score;
   }
+
+  bool IsStrike(int firstInFrame) => rolls[firstInFrame] == 10;
 
   bool IsSpare(int firstInFrame) => rolls[firstInFrame] + rolls[firstInFrame + 1] == 10;
 }
