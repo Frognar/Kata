ddcd5a87a3c58120741e7e9a39b555ce98f3b318
Frognar <sebastian.przyszlak@gmail.com>
2022-08-18 07:43:46 +0200

    all 10

--- bowling-game/bowling-game-cs/BowlingGame.Tests/06/BowlingGameTests.cs
+++ bowling-game/bowling-game-cs/BowlingGame.Tests/06/BowlingGameTests.cs
@@ -52,4 +52,10 @@ public class BowlingGameTests {
     RollMany(16, 0);
     Assert.Equal(22, game.Score());
   }
+
+  [Fact]
+  public void AllStrikes() {
+    RollMany(12, 10);
+    Assert.Equal(300, game.Score());
+  }
 }
