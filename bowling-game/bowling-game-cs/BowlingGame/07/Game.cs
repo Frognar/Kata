81574f3fc4f65932fc2ec54fe89d253150a47e08
Frognar <sebastian.przyszlak@gmail.com>
2022-08-19 15:05:04 +0200

    refactor

--- bowling-game/bowling-game-cs/BowlingGame/07/Game.cs
+++ bowling-game/bowling-game-cs/BowlingGame/07/Game.cs
@@ -1,13 +1,18 @@
 ﻿namespace BowlingGame._07; 
 
 public class Game {
-  int score;
+  readonly int[] rolls = new int[21];
+  int currentRoll;
   
   public void Roll(int pins) {
-    score += pins;
+    rolls[currentRoll++] = pins;
   }
 
   public int Score() {
+    int score = 0;
+    for (int i = 0; i < rolls.Length; i++) {
+      score += rolls[i];
+    }
     return score;
   }
 }
