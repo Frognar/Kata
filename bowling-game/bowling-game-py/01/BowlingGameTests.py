0854c8785690ee7003c5dc4e9cfd7fa5a115d096
Frognar <sebastian.przyszlak@gmail.com>
2022-08-20 08:11:08 +0200

    refactor tests

--- bowling-game/bowling-game-py/01/BowlingGameTests.py
+++ bowling-game/bowling-game-py/01/BowlingGameTests.py
@@ -3,12 +3,11 @@ from Game import Game
 
 
 class MyTestCase(unittest.TestCase):
-    def test_can_create_game(self):
-        game = Game()
+    def setUp(self) -> None:
+        self.game = Game()
 
     def test_can_roll(self):
-        game = Game()
-        game.roll(0)
+        self.game.roll(0)
 
 
 if __name__ == '__main__':
