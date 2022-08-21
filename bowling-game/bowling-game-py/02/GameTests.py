9491984f824b097869df007f8a44dc0bbf06e8f6
Frognar <sebastian.przyszlak@gmail.com>
2022-08-21 12:15:28 +0200

    all 0

--- bowling-game/bowling-game-py/02/GameTests.py
+++ bowling-game/bowling-game-py/02/GameTests.py
@@ -9,6 +9,10 @@ class BowlingGameTestCase(unittest.TestCase):
     def test_can_roll(self):
         self.game.roll(0)
 
+    def test_all_zeros(self):
+        [self.game.roll(0) for _ in range(20)]
+        assert 0 == self.game.score()
+
 
 if __name__ == '__main__':
     unittest.main()
