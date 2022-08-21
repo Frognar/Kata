b2cb4ac3f82e06a49c2236c131f7f7af69fd8b7f
Frognar <sebastian.przyszlak@gmail.com>
2022-08-21 12:22:44 +0200

    refactor tests

--- bowling-game/bowling-game-py/02/GameTests.py
+++ bowling-game/bowling-game-py/02/GameTests.py
@@ -9,6 +9,10 @@ class BowlingGameTestCase(unittest.TestCase):
     def roll_many(self, n, pins):
         [self.game.roll(pins) for _ in range(n)]
 
+    def roll_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+
     def test_all_zeros(self):
         self.roll_many(20, 0)
         assert 0 == self.game.score()
@@ -18,8 +22,7 @@ class BowlingGameTestCase(unittest.TestCase):
         assert 20 == self.game.score()
 
     def test_one_spare(self):
-        self.game.roll(5)
-        self.game.roll(5)
+        self.roll_spare()
         self.game.roll(9)
         self.roll_many(17, 0)
         assert 28 == self.game.score()
