2343659ff189ef4d86668574f4d3cae979a1dfa8
Frognar <sebastian.przyszlak@gmail.com>
2022-08-22 20:51:08 +0200

    refactor

--- bowling-game/bowling-game-py/03/Game.py
+++ bowling-game/bowling-game-py/03/Game.py
@@ -9,6 +9,9 @@ class Game:
         score = 0
         first_in_frame = 0
         for frame in range(10):
-            score += self.rolls[first_in_frame] + self.rolls[first_in_frame + 1]
+            score += self.two_balls_in_frame(first_in_frame)
             first_in_frame += 2
         return score
+
+    def two_balls_in_frame(self, first_in_frame) -> int:
+        return self.rolls[first_in_frame] + self.rolls[first_in_frame + 1]
