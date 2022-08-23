6ea57cf8d84cebe95cf8a2195f3629561b1f8a79
Frognar <sebastian.przyszlak@gmail.com>
2022-08-23 05:32:00 +0200

    refactor tests

--- bowling-game/bowling-game-py/04/GameTests.py
+++ bowling-game/bowling-game-py/04/GameTests.py
@@ -6,9 +6,6 @@ class BowlingGameTestCase(unittest.TestCase):
     def setUp(self):
         self.game = Game()
 
-    def test_can_roll(self):
-        self.game.roll(0)
-
     def test_all_zeros(self):
         [self.game.roll(0) for _ in range(20)]
         assert 0 == self.game.score()
