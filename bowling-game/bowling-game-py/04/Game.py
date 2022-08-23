bb7f1478badcc7cb5e401e02442e72b850289b13
Frognar <sebastian.przyszlak@gmail.com>
2022-08-23 05:38:00 +0200

    refactor

--- bowling-game/bowling-game-py/04/Game.py
+++ bowling-game/bowling-game-py/04/Game.py
@@ -9,7 +9,7 @@ class Game:
         score = 0
         first_in_frame = 0
         for frame in range(10):
-            if self.rolls[first_in_frame] == 10:
+            if self.is_strike(first_in_frame):
                 score += 10 + self.two_balls_in_frame(first_in_frame + 1)
                 first_in_frame += 1
             elif self.is_spare(first_in_frame):
@@ -19,6 +19,9 @@ class Game:
                 score += self.two_balls_in_frame(first_in_frame)
                 first_in_frame += 2
         return score
+
+    def is_strike(self, first_in_frame) -> bool:
+        return self.rolls[first_in_frame] == 10
 
     def is_spare(self, first_in_frame) -> bool:
         return self.two_balls_in_frame(first_in_frame) == 10
