2c2e3f6c1fae25fd5dfc174c0cb1f6e3335fc37b
Frognar <sebastian.przyszlak@gmail.com>
2022-08-23 05:31:00 +0200

    roll

--- bowling-game/bowling-game-py/04/GameTests.py
+++ bowling-game/bowling-game-py/04/GameTests.py
@@ -6,6 +6,10 @@ class BowlingGameTestCase(unittest.TestCase):
     def test_can_create_game(self):
         game = Game()
 
+    def test_can_roll(self):
+        game = Game()
+        game.roll(0)
+
 
 if __name__ == '__main__':
     unittest.main()
