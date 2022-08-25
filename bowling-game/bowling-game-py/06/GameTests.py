d07526b68b5e45f928ea9574a1f5fcced9299bf3
Frognar <sebastian.przyszlak@gmail.com>
2022-08-25 05:35:00 +0200

    spare

--- bowling-game/bowling-game-py/06/GameTests.py
+++ bowling-game/bowling-game-py/06/GameTests.py
@@ -17,6 +17,12 @@ class BowlingGameTestCase(unittest.TestCase):
         self.roll_many(20, 1)
         assert 20 == self.game.score()
 
+    def test_one_spare(self):
+        self.roll_many(2, 5)
+        self.game.roll(4)
+        self.roll_many(17, 0)
+        assert 18 == self.game.score()
+
 
 if __name__ == '__main__':
     unittest.main()
