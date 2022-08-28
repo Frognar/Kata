b917e845fec728ff756e550938298ac9596be80d
Frognar <sebastian.przyszlak@gmail.com>
2022-08-28 05:39:00 +0200

    partition strike frames

--- bowling-game/bowling-game-clj/spec/bowling_game_clj/_02/core_spec.clj
+++ bowling-game/bowling-game-clj/spec/bowling_game_clj/_02/core_spec.clj
@@ -16,6 +16,8 @@
               (should= [[1 2] [3 4]] (->frames [1 2 3 4])))
           (it "partition spare frames"
               (should= [[5 5 3] [3 4]] (->frames [5 5 3 4])))
+          (it "partition strike frames"
+              (should= [[10 5 3] [5 3]] (->frames [10 5 3])))
           )
 
 (run-specs)
