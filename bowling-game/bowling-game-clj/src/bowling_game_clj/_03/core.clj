8b2df9f6a29d52dd26448bc790313e1d072a56a9
Frognar <sebastian.przyszlak@gmail.com>
2022-08-29 05:37:30 +0200

    partition strike

--- bowling-game/bowling-game-clj/src/bowling_game_clj/_03/core.clj
+++ bowling-game/bowling-game-clj/src/bowling_game_clj/_03/core.clj
@@ -3,9 +3,10 @@
 (defn is-spare? [[first second & _]] (= 10 (+ first second)))
 
 (defn split-frame [rolls]
-  (if (is-spare? rolls)
-    [(take 3 rolls) (drop 2 rolls)]
-    [(take 2 rolls) (drop 2 rolls)]))
+  (cond
+    (= 10 (first rolls)) [(take 3 rolls) (drop 1 rolls)]
+    (is-spare? rolls) [(take 3 rolls) (drop 2 rolls)]
+    :else [(take 2 rolls) (drop 2 rolls)]))
 
 (defn ->frames [rolls]
   (if (empty? rolls)
