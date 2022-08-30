fd2fd23a0dec7c2615134168d6bee1dbb0f425c1
Frognar <sebastian.przyszlak@gmail.com>
2022-08-30 05:30:00 +0200

    init

---
+++ bowling-game/bowling-game-clj/spec/bowling_game_clj/_04/core_spec.clj
@@ -0,0 +1,5 @@
+(ns bowling-game-clj._04.core-spec
+  (:require [speclj.core :refer :all]
+            [bowling-game-clj._04.core :refer :all]))
+
+(run-specs)
---
+++ bowling-game/bowling-game-clj/src/bowling_game_clj/_04/core.clj
@@ -0,0 +1,1 @@
+(ns bowling-game-clj._04.core)
