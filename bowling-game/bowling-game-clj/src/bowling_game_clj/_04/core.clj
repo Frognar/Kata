8831b16a62b5c1a2d3dc00fe17bad684e64eb514
Frognar <sebastian.przyszlak@gmail.com>
2022-08-30 05:34:00 +0200

    refactor

--- bowling-game/bowling-game-clj/src/bowling_game_clj/_04/core.clj
+++ bowling-game/bowling-game-clj/src/bowling_game_clj/_04/core.clj
@@ -1,10 +1,13 @@
 (ns bowling-game-clj._04.core)
 
+(defn split-frame [rolls]
+  [(take 2 rolls) (drop 2 rolls)])
+
 (defn ->frames [rolls]
   (if
     (empty? rolls)
     []
-    (let [[frame remaining] [(take 2 rolls) (drop 2 rolls)]]
+    (let [[frame remaining] (split-frame rolls)]
       (cons frame (->frames remaining)))
     ))
 
