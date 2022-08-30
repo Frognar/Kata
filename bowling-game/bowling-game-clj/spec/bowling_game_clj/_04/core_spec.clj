2412415e78c13f4b620ca28f2434dc74a8289800
Frognar <sebastian.przyszlak@gmail.com>
2022-08-30 05:34:30 +0200

    partition spare

--- bowling-game/bowling-game-clj/spec/bowling_game_clj/_04/core_spec.clj
+++ bowling-game/bowling-game-clj/spec/bowling_game_clj/_04/core_spec.clj
@@ -14,6 +14,8 @@
 (describe "Frame partitioning"
           (it "partition open frames"
               (should= [[1 2] [3 4]] (->frames [1 2 3 4])))
+          (it "partition spare frames"
+              (should= [[5 5 5] [5 4]] (->frames [5 5 5 4])))
           )
 
 (run-specs)
