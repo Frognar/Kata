94aaa461eb9518de12a74d782cebf96c04f2595b
Frognar <sebastian.przyszlak@gmail.com>
2022-09-01 05:32:00 +0200

    partition open frames

--- bowling-game/bowling-game-clj/spec/bowling_game_clj/_06/core_spec.clj
+++ bowling-game/bowling-game-clj/spec/bowling_game_clj/_06/core_spec.clj
@@ -7,6 +7,13 @@
               (should= 0 (score (repeat 20 0))))
           (it "Should score 20 for all ones"
               (should= 20 (score (repeat 20 1))))
+          ;(it "Should score spare"
+          ;    (should= 20 (score [5 5 4 2])))
+          )
+
+(describe "Frame Partitioning"
+          (it "Should partition open frames"
+              (should= [[1 2] [3 4]] (->frames [1 2 3 4])))
           )
 
 (run-specs)
--- bowling-game/bowling-game-clj/src/bowling_game_clj/_06/core.clj
+++ bowling-game/bowling-game-clj/src/bowling_game_clj/_06/core.clj
@@ -1,4 +1,7 @@
 (ns bowling-game-clj._06.core)
 
+(defn ->frames [rolls]
+  (partition 2 rolls))
+
 (defn score [rolls]
   (->> rolls (apply +)))
