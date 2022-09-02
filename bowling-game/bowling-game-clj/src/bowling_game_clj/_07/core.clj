ce69b57029e154f43c71f57dd023e4b11748c5d3
Frognar <sebastian.przyszlak@gmail.com>
2022-09-02 06:02:00 +0200

    refactor

--- bowling-game/bowling-game-clj/src/bowling_game_clj/_07/core.clj
+++ bowling-game/bowling-game-clj/src/bowling_game_clj/_07/core.clj
@@ -1,10 +1,11 @@
 (ns bowling-game-clj._07.core)
 
+(defn is-strike? [rolls] (= 10 (first rolls)))
 (defn is-spare? [[first second & _]] (= 10 (+ first second)))
 
 (defn split-frames [rolls]
   (cond
-    (= 10 (first rolls)) [(take 3 rolls) (drop 1 rolls)]
+    (is-strike? rolls) [(take 3 rolls) (drop 1 rolls)]
     (is-spare? rolls) [(take 3 rolls) (drop 2 rolls)]
     :else [(take 2 rolls) (drop 2 rolls)]))
 
