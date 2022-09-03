84b7cabdca71d464bb878e8f078ff122aed0beb7
Frognar <sebastian.przyszlak@gmail.com>
2022-09-03 05:32:00 +0200

    invert null

--- name-inverter/NameInverter/NameInverter/src/test/java/pl/frognar/nameinverter/NameInverterTest.java
+++ name-inverter/NameInverter/NameInverter/src/test/java/pl/frognar/nameinverter/NameInverterTest.java
@@ -6,8 +6,8 @@ import static org.hamcrest.MatcherAssert.*;
 import org.junit.Test;
 
 public class NameInverterTest {
-    @Test
-    public void testName(){
-        assertThat(true, is(false));
+    @Test(expected = NullPointerException.class)
+    public void invert_null_shouldThrowNullPointerException(){
+        NameInverter.invert(null);
     }
 }
