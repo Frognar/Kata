476b989339aaea3be1f5ddf835d63f30b0d723b2
Frognar <sebastian.przyszlak@gmail.com>
2022-09-04 05:30:00 +0200

    init

---
+++ name-inverter/NameInverter/NameInverter/src/test/java/pl/frognar/nameinverter/_02/NameInverterTest.java
@@ -0,0 +1,10 @@
+package pl.frognar.nameinverter._02;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
+public class NameInverterTest {
+
+}
