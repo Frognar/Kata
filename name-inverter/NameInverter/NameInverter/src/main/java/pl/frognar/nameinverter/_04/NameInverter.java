975cb758ecb4ab7d5b3fca9f5e6192a15aa2c0df
Frognar <sebastian.przyszlak@gmail.com>
2022-09-06 05:45:25 +0200

    invert null throw

---
+++ name-inverter/NameInverter/NameInverter/src/main/java/pl/frognar/nameinverter/_04/NameInverter.java
@@ -0,0 +1,7 @@
+package pl.frognar.nameinverter._04;
+
+public class NameInverter {
+    public static String invert(String name) {
+        throw new NullPointerException();
+    }
+}
--- name-inverter/NameInverter/NameInverter/src/test/java/pl/frognar/nameinverter/_04/NameInverterTest.java
+++ name-inverter/NameInverter/NameInverter/src/test/java/pl/frognar/nameinverter/_04/NameInverterTest.java
@@ -6,4 +6,8 @@ import static org.hamcrest.MatcherAssert.*;
 import org.junit.Test;
 
 public class NameInverterTest {
+    @Test(expected = NullPointerException.class)
+    public void invert_null_shouldThrowNullPointerException() {
+        NameInverter.invert(null);
+    }
 }
