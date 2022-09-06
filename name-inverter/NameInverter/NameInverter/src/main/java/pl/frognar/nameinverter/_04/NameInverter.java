1f6b47f7e8a06bbafc8fa83a5165fa0733836a67
Frognar <sebastian.przyszlak@gmail.com>
2022-09-06 05:53:05 +0200

    refactor

--- name-inverter/NameInverter/NameInverter/src/main/java/pl/frognar/nameinverter/_04/NameInverter.java
+++ name-inverter/NameInverter/NameInverter/src/main/java/pl/frognar/nameinverter/_04/NameInverter.java
@@ -6,10 +6,14 @@ import java.util.List;
 
 public class NameInverter {
     public static String invert(String name) {
-        List<String> nameParts = Lists.newArrayList(name.trim().split("\\s+"));
+        List<String> nameParts = Lists.newArrayList(name.trim().split(anyAmountOfWhiteSpaces()));
         if (nameParts.size() < 2)
             return nameParts.get(0);
         return invert(nameParts);
+    }
+
+    private static String anyAmountOfWhiteSpaces() {
+        return "\\s+";
     }
 
     private static String invert(List<String> nameParts) {
