b1432940a475bb78700e38faa3972271ad8867d8
Frognar <sebastian.przyszlak@gmail.com>
2022-09-06 05:59:57 +0200

    refactor

--- name-inverter/NameInverter/NameInverter/src/test/java/pl/frognar/nameinverter/_04/NameInverterTest.java
+++ name-inverter/NameInverter/NameInverter/src/test/java/pl/frognar/nameinverter/_04/NameInverterTest.java
@@ -3,7 +3,6 @@ package pl.frognar.nameinverter._04;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class NameInverterTest {
