a46f2b8f1a187d4e02a3a135016edb6fdf237ea9
Frognar <sebastian.przyszlak@gmail.com>
2022-09-07 05:23:45 +0200

    refactor

--- name-inverter/name-inverter-java/src/main/java/pl/frognar/nameinverter/_05/NameInverter.java
+++ name-inverter/name-inverter-java/src/main/java/pl/frognar/nameinverter/_05/NameInverter.java
@@ -1,8 +1,13 @@
 package pl.frognar.nameinverter._05;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 public class NameInverter {
     public static String invert(String name) {
-        String first = name.trim();
+        List<String> nameParts = Lists.newArrayList(name.trim().split(" "));
+        String first = nameParts.get(0);
         return String.format("%s", first);
     }
 }
