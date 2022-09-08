56048b883859e9982fe107c52358bd9e7b83c437
Frognar <sebastian.przyszlak@gmail.com>
2022-09-08 05:33:57 +0200

    refactor

--- name-inverter/name-inverter-java/src/main/java/pl/frognar/nameinverter/_06/NameInverter.java
+++ name-inverter/name-inverter-java/src/main/java/pl/frognar/nameinverter/_06/NameInverter.java
@@ -10,19 +10,23 @@ public class NameInverter {
         List<String> nameParts = breakIntoPartsIgnoringWhitespaces(name);
         if (nameParts.size() < 2)
             return nameParts.get(0);
+        return invert(withoutHonorifics(nameParts));
+    }
+
+    private static ArrayList<String> breakIntoPartsIgnoringWhitespaces(String name) {
+        return Lists.newArrayList(name.trim().split(RegularExpressions.anyAmountOfWhitespaces()));
+    }
+
+    private static List<String> withoutHonorifics(List<String> nameParts) {
         if ("Mr.".equals(nameParts.get(0)))
             nameParts.remove(0);
-        return invert(nameParts);
+        return nameParts;
     }
 
     private static String invert(List<String> nameParts) {
         String first = nameParts.get(0);
         String last = nameParts.get(1);
         return String.format("%s, %s", last, first);
-    }
-
-    private static ArrayList<String> breakIntoPartsIgnoringWhitespaces(String name) {
-        return Lists.newArrayList(name.trim().split(RegularExpressions.anyAmountOfWhitespaces()));
     }
 }
 
