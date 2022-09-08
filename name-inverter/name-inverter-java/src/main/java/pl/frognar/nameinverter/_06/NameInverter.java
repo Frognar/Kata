06392a2b50848d772ec640dc4fe092abe52ea67a
Frognar <sebastian.przyszlak@gmail.com>
2022-09-08 05:26:17 +0200

    refactor

--- name-inverter/name-inverter-java/src/main/java/pl/frognar/nameinverter/_06/NameInverter.java
+++ name-inverter/name-inverter-java/src/main/java/pl/frognar/nameinverter/_06/NameInverter.java
@@ -2,8 +2,6 @@ package pl.frognar.nameinverter._06;
 
 public class NameInverter {
     public static String invert(String name) {
-        if (name == null)
-            throw new NullPointerException();
         return name.trim();
     }
 }
