8e6d0047662931754d390c6d071af5f68852ec92
Frognar <sebastian.przyszlak@gmail.com>
2022-09-08 05:31:12 +0200

    refactor

--- name-inverter/name-inverter-java/src/main/java/pl/frognar/nameinverter/_06/NameInverter.java
+++ name-inverter/name-inverter-java/src/main/java/pl/frognar/nameinverter/_06/NameInverter.java
@@ -2,16 +2,21 @@ package pl.frognar.nameinverter._06;
 
 import com.google.common.collect.Lists;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NameInverter {
     public static String invert(String name) {
-        List<String> nameParts = Lists.newArrayList(name.trim().split(RegularExpressions.anyAmountOfWhitespaces()));
+        List<String> nameParts = breakIntoPartsIgnorignWhitespaces(name);
         if (nameParts.size() < 2)
             return nameParts.get(0);
         String first = nameParts.get(0);
         String last = nameParts.get(1);
         return String.format("%s, %s", last, first);
+    }
+
+    private static ArrayList<String> breakIntoPartsIgnorignWhitespaces(String name) {
+        return Lists.newArrayList(name.trim().split(RegularExpressions.anyAmountOfWhitespaces()));
     }
 }
 
