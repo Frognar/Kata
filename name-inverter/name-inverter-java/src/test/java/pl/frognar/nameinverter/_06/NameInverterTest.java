e46dea772f1d8e24add73b8658ae3cbedf3e0c9c
Frognar <sebastian.przyszlak@gmail.com>
2022-09-08 05:27:54 +0200

    refactor

--- name-inverter/name-inverter-java/src/test/java/pl/frognar/nameinverter/_06/NameInverterTest.java
+++ name-inverter/name-inverter-java/src/test/java/pl/frognar/nameinverter/_06/NameInverterTest.java
@@ -3,6 +3,7 @@ package pl.frognar.nameinverter._06;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class NameInverterTest {
@@ -24,5 +25,11 @@ public class NameInverterTest {
     @Test
     public void invert_first_shouldReturnFirst() {
         assertThat(NameInverter.invert("John"), is("John"));
+    }
+
+    @Ignore
+    @Test
+    public void invert_firstLast_shouldReturnLastCommaFirst() {
+        assertThat(NameInverter.invert("John Smith"), is("Smith, John"));
     }
 }
