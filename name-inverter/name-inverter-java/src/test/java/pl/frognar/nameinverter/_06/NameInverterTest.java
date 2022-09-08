4a288956bf669b30ee51533ab2160dac5b0f9ded
Frognar <sebastian.przyszlak@gmail.com>
2022-09-08 05:30:10 +0200

    invert first    last -> last, first

--- name-inverter/name-inverter-java/src/test/java/pl/frognar/nameinverter/_06/NameInverterTest.java
+++ name-inverter/name-inverter-java/src/test/java/pl/frognar/nameinverter/_06/NameInverterTest.java
@@ -30,4 +30,9 @@ public class NameInverterTest {
     public void invert_firstLast_shouldReturnLastCommaFirst() {
         assertThat(NameInverter.invert("John Smith"), is("Smith, John"));
     }
+
+    @Test
+    public void invert_firstWhitespacesLast_shouldReturnLastCommaFirst() {
+        assertThat(NameInverter.invert("John   Smith"), is("Smith, John"));
+    }
 }
