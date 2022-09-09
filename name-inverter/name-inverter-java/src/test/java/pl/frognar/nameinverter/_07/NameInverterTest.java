1d00d64a6a53d70f77e9c0b59c443826f9d8aadb
Frognar <sebastian.przyszlak@gmail.com>
2022-09-09 05:31:44 +0200

    invert "" -> ""

--- name-inverter/name-inverter-java/src/test/java/pl/frognar/nameinverter/_07/NameInverterTest.java
+++ name-inverter/name-inverter-java/src/test/java/pl/frognar/nameinverter/_07/NameInverterTest.java
@@ -2,9 +2,17 @@ package pl.frognar.nameinverter._07;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
 public class NameInverterTest {
     @Test(expected = NullPointerException.class)
     public void invert_null_shouldThrowNullPointerException() {
         NameInverter.invert(null);
+    }
+
+    @Test
+    public void invert_empty_shouldReturnEmpty() {
+        assertThat(NameInverter.invert(""), is(""));
     }
 }
