f98e84a2f6e06af4efdb2c928352261d69b2a488
Frognar <sebastian.przyszlak@gmail.com>
2022-09-10 21:03:24 +0200

    final test

--- name-inverter/name-inverter-py/NameInverterTests.py
+++ name-inverter/name-inverter-py/NameInverterTests.py
@@ -32,6 +32,9 @@ class NameInverterTestCase(unittest.TestCase):
     def test_invert_first_last_postnominals_should_return_last_comma_first_postnominals(self):
         self.assertEqual(invert('Horus Lupercal Sr. PhD.'), 'Lupercal, Horus Sr. PhD.')
 
+    def test_final(self):
+        self.assertEqual(invert(' Mr.   Horus   Lupercal  Sr.    PhD.'), 'Lupercal, Horus Sr. PhD.')
+
 
 if __name__ == '__main__':
     unittest.main()
