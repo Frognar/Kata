867709c37f32cdbdeb972d7368e98fef5428f6c3
Frognar <sebastian.przyszlak@gmail.com>
2022-09-10 20:47:33 +0200

    refactor

--- name-inverter/name-inverter-py/NameInverter.py
+++ name-inverter/name-inverter-py/NameInverter.py
@@ -2,12 +2,16 @@ import re
 
 
 def invert(name):
-    name_parts = re.split(any_amount_of_whitespaces(), name.strip())
+    name_parts = break_into_parts_ignoring_whitespaces(name)
     if len(name_parts) < 2:
         return name_parts[0]
     first = name_parts[0]
     last = name_parts[1]
     return f'{last}, {first}'
+
+
+def break_into_parts_ignoring_whitespaces(name):
+    return re.split(any_amount_of_whitespaces(), name.strip())
 
 
 def any_amount_of_whitespaces():
