2f65d7b98f90d0e2c71ee729c8d5048ee07f85bf
Frognar <sebastian.przyszlak@gmail.com>
2022-09-11 09:27:58 +0200

    invert 'Mr. Horus Lupercal' -> 'Lupercal, Horus'

--- name-inverter/name-inverter-py/02/NameInverter.py
+++ name-inverter/name-inverter-py/02/NameInverter.py
@@ -7,17 +7,19 @@ class NameInverter:
         name_parts = NameInverter.break_into_parts_ignoring_whitespaces(name)
         if len(name_parts) < 2:
             return name_parts[0]
+        if 'Mr.' == name_parts[0]:
+            name_parts.pop(0)
         return NameInverter.invert_parts(name_parts)
+
+    @staticmethod
+    def break_into_parts_ignoring_whitespaces(name):
+        return re.split(RegularExpressions.any_amount_of_whitespaces(), name.strip())
 
     @staticmethod
     def invert_parts(name_parts):
         first = name_parts[0]
         last = name_parts[1]
         return f'{last}, {first}'
-
-    @staticmethod
-    def break_into_parts_ignoring_whitespaces(name):
-        return re.split(RegularExpressions.any_amount_of_whitespaces(), name.strip())
 
 
 class RegularExpressions:
