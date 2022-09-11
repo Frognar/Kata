2ded2b3bf3095756a6651027721345b8fd19385e
Frognar <sebastian.przyszlak@gmail.com>
2022-09-11 09:19:53 +0200

    refactor

--- name-inverter/name-inverter-py/02/NameInverter.py
+++ name-inverter/name-inverter-py/02/NameInverter.py
@@ -2,5 +2,7 @@ class NameInverter:
     @staticmethod
     def invert(name):
         name_parts = name.strip().split(' ')
+        if len(name_parts) < 2:
+            return name_parts[0]
         first = name_parts[0]
         return f'{first}'
