c32c5aa8f0d6c9e81e127d07df8a02b973e48871
Frognar <sebastian.przyszlak@gmail.com>
2022-09-11 09:29:08 +0200

    refactor

--- name-inverter/name-inverter-py/02/NameInverter.py
+++ name-inverter/name-inverter-py/02/NameInverter.py
@@ -7,13 +7,17 @@ class NameInverter:
         name_parts = NameInverter.break_into_parts_ignoring_whitespaces(name)
         if len(name_parts) < 2:
             return name_parts[0]
-        if 'Mr.' == name_parts[0]:
-            name_parts.pop(0)
-        return NameInverter.invert_parts(name_parts)
+        return NameInverter.invert_parts(NameInverter.without_honorifics(name_parts))
 
     @staticmethod
     def break_into_parts_ignoring_whitespaces(name):
         return re.split(RegularExpressions.any_amount_of_whitespaces(), name.strip())
+
+    @staticmethod
+    def without_honorifics(name_parts):
+        if 'Mr.' == name_parts[0]:
+            name_parts.pop(0)
+        return name_parts
 
     @staticmethod
     def invert_parts(name_parts):
