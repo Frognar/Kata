91f109e4cc1d8e059ffa1ef9ba2747f670e386e3
Frognar <sebastian.przyszlak@gmail.com>
2022-09-11 09:24:43 +0200

    refactor

--- name-inverter/name-inverter-py/02/NameInverter.py
+++ name-inverter/name-inverter-py/02/NameInverter.py
@@ -4,7 +4,7 @@ import re
 class NameInverter:
     @staticmethod
     def invert(name):
-        name_parts = re.split(NameInverter.any_amount_of_whitespaces(), name.strip())
+        name_parts = re.split(RegularExpressions.any_amount_of_whitespaces(), name.strip())
         if len(name_parts) < 2:
             return name_parts[0]
         return NameInverter.invert_parts(name_parts)
@@ -15,6 +15,8 @@ class NameInverter:
         last = name_parts[1]
         return f'{last}, {first}'
 
+
+class RegularExpressions:
     @staticmethod
     def any_amount_of_whitespaces():
         return '\\s+'
