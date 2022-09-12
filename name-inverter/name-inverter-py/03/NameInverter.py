fd0a73ac809ed73369853962a949985e111adbc6
Frognar <sebastian.przyszlak@gmail.com>
2022-09-12 05:53:45 +0200

    refactor

--- name-inverter/name-inverter-py/03/NameInverter.py
+++ name-inverter/name-inverter-py/03/NameInverter.py
@@ -3,14 +3,14 @@ from typing import List
 
 class NameInverter:
     @classmethod
-    def invert(cls, name) -> str:
+    def invert(cls, name: str) -> str:
         name_parts = cls.break_into_parts(name)
         if len(name_parts) < 2:
             return name_parts[0]
         return cls.invert_parts(name_parts)
 
     @classmethod
-    def break_into_parts(cls, name) -> List[str]:
+    def break_into_parts(cls, name: str) -> List[str]:
         return name.strip().split(cls.space())
 
     @classmethod
@@ -18,7 +18,7 @@ class NameInverter:
         return ' '
 
     @classmethod
-    def invert_parts(cls, name_parts) -> str:
+    def invert_parts(cls, name_parts: List[str]) -> str:
         first = name_parts[0]
         last = name_parts[1]
         return f'{last}, {first}'
