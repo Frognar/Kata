5e96b62bf0d7c3e5421d92aa5e6a2f2ff93391ef
Frognar <sebastian.przyszlak@gmail.com>
2022-09-12 05:57:50 +0200

    invert 'Mr. Horus Lupercal' -> 'Lupercal, Horus'

--- name-inverter/name-inverter-py/03/NameInverterTest.py
+++ name-inverter/name-inverter-py/03/NameInverterTest.py
@@ -21,6 +21,9 @@ class NameInverterTestTestCase(unittest.TestCase):
     def test_invert_first_whitespaces_last_should_return_last_comma_first(self):
         self.assertEqual(NameInverter.invert('Horus   Lupercal'), 'Lupercal, Horus')
 
+    def test_invert_honorific_first_last_should_return_last_comma_first(self):
+        self.assertEqual(NameInverter.invert('Mr. Horus Lupercal'), 'Lupercal, Horus')
+
 
 if __name__ == '__main__':
     unittest.main()
