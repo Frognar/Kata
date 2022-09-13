787b5580342e8dfc8746edc99d83a0e35c3c54a2
Frognar <sebastian.przyszlak@gmail.com>
2022-09-13 04:59:17 +0200

    refactor

--- name-inverter/name-inverter-py/04/NameInverterTest.py
+++ name-inverter/name-inverter-py/04/NameInverterTest.py
@@ -15,6 +15,9 @@ class NameInverterTestCase(unittest.TestCase):
     def test_invert_firstname_should_return_firstname(self):
         self.assertEqual(NameInverter.invert('Horus'), 'Horus')
 
+    def _test_invert_first_last_should_return_last_comma_first(self):
+        self.assertEqual(NameInverter.invert('Horus Lupercal'), 'Lupercal, Horus')
+
 
 if __name__ == '__main__':
     unittest.main()
