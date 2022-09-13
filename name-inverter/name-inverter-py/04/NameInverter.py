daaf7c6744a8b48415e7c76a50b81e6b11e6350a
Frognar <sebastian.przyszlak@gmail.com>
2022-09-13 05:04:11 +0200

    refactor

--- name-inverter/name-inverter-py/04/NameInverter.py
+++ name-inverter/name-inverter-py/04/NameInverter.py
@@ -5,14 +5,18 @@ from typing import List
 class NameInverter:
     @classmethod
     def invert(cls, name: str) -> str:
-        name_parts = cls.break_into_parts(name)
+        name_parts = cls.break_into_parts_ignoring_whitespaces(name)
         if len(name_parts) < 2:
             return name_parts[0]
         return cls.invert_parts(name_parts)
 
     @classmethod
-    def break_into_parts(cls, name: str) -> List[str]:
-        return re.split('\\s+', name.strip())
+    def break_into_parts_ignoring_whitespaces(cls, name: str) -> List[str]:
+        return re.split(cls.any_amount_of_whitespaces(), name.strip())
+
+    @classmethod
+    def any_amount_of_whitespaces(cls) -> str:
+        return '\\s+'
 
     @classmethod
     def invert_parts(cls, name_parts: List[str]) -> str:
