dcadfac3850678e0437efcc51c07030767eb553a
Frognar <sebastian.przyszlak@gmail.com>
2022-09-13 04:57:38 +0200

    invert '   ' -> ''

--- name-inverter/name-inverter-py/04/NameInverterTest.py
+++ name-inverter/name-inverter-py/04/NameInverterTest.py
@@ -9,6 +9,9 @@ class NameInverterTestCase(unittest.TestCase):
     def test_invert_empty_should_return_empty(self):
         self.assertEqual(NameInverter.invert(''), '')
 
+    def test_invert_whitespaces_should_return_empty(self):
+        self.assertEqual(NameInverter.invert('   '), '')
+
 
 if __name__ == '__main__':
     unittest.main()
