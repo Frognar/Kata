69bc7c3516c1f1bca85ec8d8e78bae066605d9e1
Frognar <sebastian.przyszlak@gmail.com>
2022-09-14 04:54:18 +0200

    invert 'Horus Lupercal' -> 'Lupercal, Horus'

--- name-inverter/name-inverter-py/05/NameInverter.py
+++ name-inverter/name-inverter-py/05/NameInverter.py
@@ -2,5 +2,8 @@ class NameInverter:
     @classmethod
     def invert(cls, name: str) -> str:
         name_parts = name.strip().split(' ')
+        if len(name_parts) < 2:
+            return name_parts[0]
         first = name_parts[0]
-        return f'{first}'
+        last = name_parts[1]
+        return f'{last}, {first}'
