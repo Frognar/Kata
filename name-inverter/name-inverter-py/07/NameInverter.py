12d8bccd403c074213940057ed53fe027da0416d
Frognar <sebastian.przyszlak@gmail.com>
2022-09-16 05:49:10 +0200

    refactor

--- name-inverter/name-inverter-py/07/NameInverter.py
+++ name-inverter/name-inverter-py/07/NameInverter.py
@@ -34,7 +34,8 @@ class RegularExpressions:
 
 
 class Honorifics:
-    @staticmethod
-    def is_honorific(s: str) -> bool:
-        known_honorifics = ['Mr.']
-        return s in known_honorifics
+    known_honorifics = ['Mr.']
+
+    @classmethod
+    def is_honorific(cls, s: str) -> bool:
+        return s in cls.known_honorifics
