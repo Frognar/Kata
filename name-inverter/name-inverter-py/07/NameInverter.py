8142332fb804fbb9142ee49d582b6d9653c6809f
Frognar <sebastian.przyszlak@gmail.com>
2022-09-16 05:44:23 +0200

    invert 'Horus    Lupercal' -> 'Lupercal, Horus'

--- name-inverter/name-inverter-py/07/NameInverter.py
+++ name-inverter/name-inverter-py/07/NameInverter.py
@@ -1,17 +1,18 @@
+import re
 from typing import List
 
 
 class NameInverter:
     @classmethod
     def invert(cls, name: str) -> str:
-        name_parts = cls.break_into_parts(name)
+        name_parts = cls.break_into_parts_ignoring_whitespaces(name)
         if len(name_parts) < 2:
             return name_parts[0]
         return cls.invert_parts(name_parts)
 
     @staticmethod
-    def break_into_parts(name: str) -> List[str]:
-        return name.strip().split(' ')
+    def break_into_parts_ignoring_whitespaces(name: str) -> List[str]:
+        return re.split('\\s+', name.strip())
 
     @classmethod
     def invert_parts(cls, name_parts: List[str]) -> str:
