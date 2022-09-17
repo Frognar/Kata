406592d2bf66144c81f2aec6db8a14176b2a212a
Frognar <sebastian.przyszlak@gmail.com>
2022-09-17 12:58:41 +0200

    refactor

--- name-inverter/name-inverter-clj/src/name_inverter_clj/_01/core.clj
+++ name-inverter/name-inverter-clj/src/name_inverter_clj/_01/core.clj
@@ -4,12 +4,15 @@
 (defn break-into-parts [name]
   (str/split name #" "))
 
+(defn invert-parts [name_parts]
+  (str (second name_parts) ", " (first name_parts)))
+
 (defn invert [name]
     (let [name_parts (break-into-parts name)]
       (cond
         (< (count name_parts) 1) ""
         (< (count name_parts) 2) (first name_parts)
-        :else (str (second name_parts) ", " (first name_parts))
+        :else (invert-parts name_parts)
         )
-    )
+      )
   )
