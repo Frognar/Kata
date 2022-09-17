c148cfb38ecb365e0fac18da94e5f71384a5cd19
Frognar <sebastian.przyszlak@gmail.com>
2022-09-17 13:05:19 +0200

    refactor

--- name-inverter/name-inverter-clj/src/name_inverter_clj/_01/core.clj
+++ name-inverter/name-inverter-clj/src/name_inverter_clj/_01/core.clj
@@ -10,12 +10,15 @@
 (defn invert-parts [name_parts]
   (str (second name_parts) ", " (first name_parts)))
 
+(defn is-honorific? [s]
+  (= "Mr." s))
+
 (defn invert [name]
     (let [name_parts (break-into-parts-ignoring-whitespaces name)]
       (cond
         (< (count name_parts) 1) ""
         (< (count name_parts) 2) (first name_parts)
-        :else (invert-parts (remove #(= "Mr." %) name_parts))
+        :else (invert-parts (remove #(is-honorific? %) name_parts))
         )
       )
   )
