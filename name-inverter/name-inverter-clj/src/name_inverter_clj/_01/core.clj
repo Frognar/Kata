0a013477a315eaa429fc66a7195b4a24b7c26da2
Frognar <sebastian.przyszlak@gmail.com>
2022-09-17 13:21:28 +0200

    refactor

--- name-inverter/name-inverter-clj/src/name_inverter_clj/_01/core.clj
+++ name-inverter/name-inverter-clj/src/name_inverter_clj/_01/core.clj
@@ -6,10 +6,13 @@
 (defn break-into-parts-ignoring-whitespaces [name]
   (str/split (str/trim name) any-amount-of-whitespaces))
 
+(defn find-postnominals [name_parts]
+  (if (< (count name_parts) 3) "" (nth name_parts 2)))
+
 (defn invert-parts [name_parts]
   (let [first (first name_parts)
         last (second name_parts)
-        postnominals (if (< (count name_parts) 3) "" (nth name_parts 2))]
+        postnominals (find-postnominals name_parts)]
     (str/trim (str last ", " first " " postnominals))
   ))
 
