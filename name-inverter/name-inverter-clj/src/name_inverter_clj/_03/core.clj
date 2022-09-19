2a73983795ddebe694e9fcbce0cb1e2171c4c946
Frognar <sebastian.przyszlak@gmail.com>
2022-09-19 05:17:59 +0200

    refactor

--- name-inverter/name-inverter-clj/src/name_inverter_clj/_03/core.clj
+++ name-inverter/name-inverter-clj/src/name_inverter_clj/_03/core.clj
@@ -5,6 +5,11 @@
   (let [[first last] [(first name_parts) (second name_parts)]]
     (str last ", " first)))
 
+(defn without-honorifics [name_parts]
+  (if (= (first name_parts) "Mr.")
+    (drop 1 name_parts)
+    name_parts))
+
 (def any-amount-of-whitespaces #"\s+")
 
 (defn break-into-parts-ignoring-whitespaces [name]
@@ -14,6 +19,4 @@
     (let [name_parts (break-into-parts-ignoring-whitespaces name)]
       (if (< (count name_parts) 2)
         (first name_parts)
-        (if (= (first name_parts) "Mr.")
-          (invert-parts (drop 1 name_parts))
-          (invert-parts name_parts)))))
+        (invert-parts (without-honorifics name_parts)))))
