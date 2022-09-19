be42a4eb7378d5d33da5c62add9767c828d24b5c
Frognar <sebastian.przyszlak@gmail.com>
2022-09-19 05:08:07 +0200

    invert nil should throw exception

--- name-inverter/name-inverter-clj/spec/name_inverter_clj/_03/core_spec.clj
+++ name-inverter/name-inverter-clj/spec/name_inverter_clj/_03/core_spec.clj
@@ -2,6 +2,7 @@
   (:require [speclj.core :refer :all]
             [name-inverter-clj._03.core :refer :all]))
 
-(describe "test"
-          (it "should fail"
-              (should= true false)))
+(describe "Name Inverter"
+          (it "Invert nil should throw exception"
+              (should-throw (invert nil)))
+          )
--- name-inverter/name-inverter-clj/src/name_inverter_clj/_03/core.clj
+++ name-inverter/name-inverter-clj/src/name_inverter_clj/_03/core.clj
@@ -1,1 +1,4 @@
 (ns name-inverter-clj._03.core)
+
+(defn invert [name]
+  (throw NullPointerException))
