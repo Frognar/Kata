edc0d2b573282c6cff30aaabbde6e4c7afece84e
Frognar <sebastian.przyszlak@gmail.com>
2022-09-20 05:32:39 +0200

    init

---
+++ name-inverter/name-inverter-clj/spec/name_inverter_clj/_04/core_spec.clj
@@ -0,0 +1,7 @@
+(ns name-inverter-clj._04.core-spec
+  (:require [speclj.core :refer :all]
+            [name-inverter-clj._04.core :refer :all]))
+
+(describe "test"
+          (it "should fail"
+              (should= true false)))
