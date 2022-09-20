91d84977ed55510681054f22b876ebc8ba253f7a
Frognar <sebastian.przyszlak@gmail.com>
2022-09-20 05:34:51 +0200

    refactor

--- name-inverter/name-inverter-clj/src/name_inverter_clj/_04/core.clj
+++ name-inverter/name-inverter-clj/src/name_inverter_clj/_04/core.clj
@@ -1,6 +1,7 @@
-(ns name-inverter-clj._04.core)
+(ns name-inverter-clj._04.core
+  (:require [clojure.string :as str]))
 
 (defn invert [name]
   (if (nil? name)
     (throw NullPointerException)
-    ""))
+    (str/trim name)))
