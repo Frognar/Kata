54f51f534c8b69402db42d9bbe33f50571eae6af
Frognar <sebastian.przyszlak@gmail.com>
2022-09-21 05:42:12 +0200

    refactor

--- name-inverter/name-inverter-clj/src/name_inverter_clj/_05/core.clj
+++ name-inverter/name-inverter-clj/src/name_inverter_clj/_05/core.clj
@@ -1,9 +1,14 @@
 (ns name-inverter-clj._05.core
   (:require [clojure.string :as str]))
 
+(defn find-postnominals [name_parts]
+  (if (< (count name_parts) 3)
+    ""
+    (nth name_parts 2)))
+
 (defn invert-parts [name_parts]
   (let [[first last] [(first name_parts) (second name_parts)]
-        postnominals (if (< (count name_parts) 3) "" (nth name_parts 2))]
+        postnominals (find-postnominals name_parts)]
     (str/trim (str last ", " first " " postnominals))))
 
 (def known-honorifics #{"Mr.", "Mrs."})
