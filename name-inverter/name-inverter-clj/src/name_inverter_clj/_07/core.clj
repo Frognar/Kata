14949f48b2670af90d207041e891f8da69e55f4e
Frognar <sebastian.przyszlak@gmail.com>
2022-09-23 05:38:16 +0200

    refactor

--- name-inverter/name-inverter-clj/src/name_inverter_clj/_07/core.clj
+++ name-inverter/name-inverter-clj/src/name_inverter_clj/_07/core.clj
@@ -1,6 +1,9 @@
 (ns name-inverter-clj._07.core
   (:require [clojure.string :as str]))
 
+(defn break-into-parts [name]
+  (str/split (str/trim name) #" "))
+
 (defn invert [name]
-  (let [name_parts (str/split (str/trim name) #" ")]
+  (let [name_parts (break-into-parts name)]
     (first name_parts)))
