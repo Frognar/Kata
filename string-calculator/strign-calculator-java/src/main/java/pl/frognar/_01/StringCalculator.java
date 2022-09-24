b7a0475854b31097268a525e0fc6f68110348179
Frognar <sebastian.przyszlak@gmail.com>
2022-09-24 22:40:34 +0200

    add "2" -> 2

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_01/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_01/StringCalculator.java
@@ -6,6 +6,6 @@ public class StringCalculator {
             return 0;
         }
 
-        return 1;
+        return Integer.parseInt(numbers);
     }
 }
--- string-calculator/strign-calculator-java/src/test/java/pl/frognar/_01/StringCalculatorTest.java
+++ string-calculator/strign-calculator-java/src/test/java/pl/frognar/_01/StringCalculatorTest.java
@@ -1,8 +1,14 @@
 package pl.frognar._01;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class StringCalculatorTest {
     @Test
@@ -11,9 +17,17 @@ public class StringCalculatorTest {
         assertEquals(0, calculator.add(""));
     }
 
-    @Test
-    public void shouldReturnNumberForStringWithOneNumber() {
+    static Stream<Arguments> stringWithOneNumber() {
+        return Stream.of(
+                arguments("1", 1),
+                arguments("2", 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringWithOneNumber")
+    public void shouldReturnNumberForStringWithOneNumber(String numbers, int expectedValue) {
         StringCalculator calculator = new StringCalculator();
-        assertEquals(1, calculator.add("1"));
+        assertEquals(expectedValue, calculator.add(numbers));
     }
 }
