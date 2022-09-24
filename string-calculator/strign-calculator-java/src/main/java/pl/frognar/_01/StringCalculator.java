1e072759f28c705a0e6a83b2e42e30a0ba3ded5d
Frognar <sebastian.przyszlak@gmail.com>
2022-09-24 23:02:37 +0200

    refactor

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_01/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_01/StringCalculator.java
@@ -6,15 +6,15 @@ public class StringCalculator {
             return 0;
         }
 
-        String delimiters = "[,\\n]";
+        String delimiters = ",\\n";
         if (numbers.startsWith("//")) {
             int indexOfCustomDelimiterEnd = numbers.indexOf('\n');
             String customDelimiter = numbers.substring(2, indexOfCustomDelimiterEnd);
-            delimiters = "[" + customDelimiter + delimiters.substring(1);
+            delimiters += customDelimiter;
             numbers = numbers.substring(indexOfCustomDelimiterEnd + 1);
         }
 
-        String[] numbersList = numbers.split(delimiters);
+        String[] numbersList = numbers.split("[%s]".formatted(delimiters));
         int sum = 0;
         for (String n: numbersList) {
             sum += Integer.parseInt(n);
