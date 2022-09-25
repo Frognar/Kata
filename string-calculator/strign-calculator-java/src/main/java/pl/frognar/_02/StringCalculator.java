e3bc9b93f1f2fa94b7afb4f9a2ca8accd216c9bb
Frognar <sebastian.przyszlak@gmail.com>
2022-09-25 13:26:05 +0200

    refactor

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_02/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_02/StringCalculator.java
@@ -3,15 +3,14 @@ package pl.frognar._02;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        numbers = numbers.replaceAll("[^0-9-]", ",");
-        String delimiters = ",";
-        var numbersList = convertToInts(numbers.split(delimiters));
+        var numbersList = convertToInts(splitAndFilterNumbers(numbers));
         var negatives = numbersList.stream().filter(n -> n < 0).toList();
         if (!negatives.isEmpty()) {
             String negativesString = negatives
@@ -25,9 +24,14 @@ public class StringCalculator {
         return numbersList.stream().reduce(0, Integer::sum);
     }
 
-    List<Integer> convertToInts(String[] numbers) {
-        return Arrays.stream(numbers)
-                .filter(s -> !s.isEmpty())
+    Stream<String> splitAndFilterNumbers(String numbers) {
+        return Arrays.stream(numbers.replaceAll("[^0-9-]", ",")
+                .split(","))
+                .filter(s -> !s.isEmpty());
+    }
+
+    List<Integer> convertToInts(Stream<String> numbers) {
+        return numbers
                 .map(Integer::parseInt)
                 .toList();
     }
