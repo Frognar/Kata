099f3518ceb3ac38530c0b69852dfd8c56c95fe7
Frognar <sebastian.przyszlak@gmail.com>
2022-09-25 12:42:41 +0200

    refactor

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_01/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_01/StringCalculator.java
@@ -1,68 +1,37 @@
 package pl.frognar._01;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringCalculator {
-    private final String customDelimiterRegex = "//(.+)\n";
-    private final Pattern customDelimiterPattern = Pattern.compile(customDelimiterRegex);
-    private final String extractDelimiterRegex = "\\[?([^]]+)]?";
-    private final Pattern extractDelimiterPattern = Pattern.compile(extractDelimiterRegex);
-
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
 
-        numbers = numbers.replaceAll("[*+]", "A");
-        String delimiters = "[,\\n]";
-        Matcher customDelimiterMatcher = customDelimiterPattern.matcher(numbers);
-        if (customDelimiterMatcher.find()) {
-            delimiters += "%s".formatted(mergeCustomDelimiter(customDelimiterMatcher));
-            numbers = skipCustomDelimiter(numbers);
-        }
-
-        return calculateSumOf(numbers.split(delimiters));
+        numbers = numbers.replaceAll("[^0-9-]+", ",");
+        Stream<Integer> streamOfNumbers =
+                Arrays.stream(numbers.split(","))
+                        .filter(s -> !s.isEmpty())
+                        .map(Integer::parseInt);
+        return calculateSumOf(streamOfNumbers.toList());
     }
 
-    private String mergeCustomDelimiter(Matcher customDelimiterMatcher) {
-        String delimitersText = customDelimiterMatcher.group(1);
-        Matcher extractDelimiterMatcher = extractDelimiterPattern.matcher(delimitersText);
-        StringBuilder customDelimiters = new StringBuilder();
-        while (extractDelimiterMatcher.find()) {
-            customDelimiters.append("|").append(extractDelimiterMatcher.group(1));
-        }
-
-        return customDelimiters.toString();
-    }
-
-    private String skipCustomDelimiter(String numbers) {
-        return customDelimiterPattern.split(numbers)[1];
-    }
-
-    private static int calculateSumOf(String[] numbers) {
-        List<Integer> negatives = new ArrayList<>();
-        int sum = 0;
-        for (String n : numbers) {
-            int number = Integer.parseInt(n);
-            if (number < 0) {
-                negatives.add(number);
-            }
-
-            if (number <= 1000) {
-                sum += number;
-            }
-        }
-
+    private static int calculateSumOf(List<Integer> numbers) {
+        var negatives = numbers.stream().filter(n -> n < 0).toList();
         if (!negatives.isEmpty()) {
-            String negativesString = negatives.stream().map(String::valueOf).collect(Collectors.joining(","));
+            String negativesString = negatives
+                    .stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(","));
             throw new NegativeNumbersNotAllowedException(
                     "negatives not allowed: %s".formatted(negativesString));
         }
 
-        return sum;
+        return numbers.stream()
+                .filter(n -> n <= 1000)
+                .reduce(0, Integer::sum);
     }
 }
