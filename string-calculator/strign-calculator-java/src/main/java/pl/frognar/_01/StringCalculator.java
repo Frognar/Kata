6341997ed10c6b509397a35e8ee20ee55727d89f
Frognar <sebastian.przyszlak@gmail.com>
2022-09-25 21:24:33 +0200

    refactor

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_01/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_01/StringCalculator.java
@@ -1,7 +1,10 @@
 package pl.frognar._01;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,11 +13,9 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-
-        numbers = numbers.replaceAll("[^0-9-]+", ",");
+        numbers = replaceAllCustomDelimitersToCommas(numbers);
         Stream<Integer> streamOfNumbers =
-                Arrays.stream(numbers.split(","))
-                        .filter(s -> !s.isEmpty())
+                Arrays.stream(numbers.split("[,\n]"))
                         .map(Integer::parseInt);
         return calculateSumOf(streamOfNumbers.toList());
     }
@@ -33,5 +34,35 @@ public class StringCalculator {
         return numbers.stream()
                 .filter(n -> n <= 1000)
                 .reduce(0, Integer::sum);
+    }
+
+    private static final Pattern customDelimitersPattern = Pattern.compile("//(\\[?.+]?)+\n");
+    static String replaceAllCustomDelimitersToCommas(String numbers) {
+        Matcher multipleMatcher = customDelimitersPattern.matcher(numbers);
+        if (multipleMatcher.find()) {
+            List<String> delimiters = getAllCustomDelimiters(multipleMatcher.group(0));
+            numbers = replaceAllDelimitersWithComma(numbers, delimiters);
+            return customDelimitersPattern.split(numbers)[1];
+        }
+
+        return numbers;
+    }
+
+    private static final String escapedRegex = "\\Q%s\\E";
+    private static List<String> getAllCustomDelimiters(String delimitersGroup) {
+        delimitersGroup = delimitersGroup.substring(2, delimitersGroup.length() - 1);
+        delimitersGroup = delimitersGroup.replaceAll(escapedRegex.formatted("]["), " ");
+        delimitersGroup = delimitersGroup.replaceAll(escapedRegex.formatted("["), "");
+        delimitersGroup = delimitersGroup.replaceAll(escapedRegex.formatted("]"), "");
+        return Arrays.stream(delimitersGroup.split(" "))
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .toList();
+    }
+
+    private static String replaceAllDelimitersWithComma(String originalNumbers, List<String> delimiters) {
+        for (var delimiter : delimiters) {
+            originalNumbers = originalNumbers.replaceAll(escapedRegex.formatted(delimiter), ",");
+        }
+        return originalNumbers;
     }
 }
