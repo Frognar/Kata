1de6f0e547431383e53b3bf0d265e816ba95a66e
Frognar <sebastian.przyszlak@gmail.com>
2022-09-26 05:03:41 +0200

    add "1" -> 1

--- string-calculator/strign-calculator-java/src/test/java/pl/frognar/_03/StringCalculatorTest.java
+++ string-calculator/strign-calculator-java/src/test/java/pl/frognar/_03/StringCalculatorTest.java
@@ -10,4 +10,10 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void addShouldReturnNumberForStringWithOneNumber() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("1"));
+    }
 }
