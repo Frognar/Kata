b37470431440df579e17a8bb5aab8c3e4d1faf37
Frognar <sebastian.przyszlak@gmail.com>
2022-09-26 05:02:24 +0200

    add "" -> 0

--- string-calculator/strign-calculator-java/src/test/java/pl/frognar/_03/StringCalculatorTest.java
+++ string-calculator/strign-calculator-java/src/test/java/pl/frognar/_03/StringCalculatorTest.java
@@ -2,11 +2,12 @@ package pl.frognar._03;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
     @Test
-    public void shouldFail() {
-        fail();
+    public void addShouldReturnZeroForEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
     }
 }
