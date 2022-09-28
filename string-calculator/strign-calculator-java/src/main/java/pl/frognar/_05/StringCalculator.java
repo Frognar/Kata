163d4d31aabf919a2352f0c721087571546a32cf
Frognar <sebastian.przyszlak@gmail.com>
2022-09-28 05:52:48 +0200

    refactor

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_05/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_05/StringCalculator.java
@@ -1,13 +1,14 @@
 package pl.frognar._05;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers == null || numbers.isEmpty())
             return 0;
-        var numberList = splitAndConvertNumbers(numbers).toList();
+        var numberList = splitAndConvertNumbers(numbers);
         var negatives = numberList.stream().filter(n -> n < 0);
         if (negatives.findAny().isPresent()) {
             throw new NegativeNumbersNotAllowedException("negatives not allowed: -1");
@@ -17,9 +18,10 @@ public class StringCalculator {
     }
 
     private static final String defaultDelimiters = "[,\n]";
-    private static Stream<Integer> splitAndConvertNumbers(String numbers) {
+    private static List<Integer> splitAndConvertNumbers(String numbers) {
         return Arrays.stream(numbers.split(defaultDelimiters))
-                .map(Integer::parseInt);
+                .map(Integer::parseInt)
+                .toList();
     }
 
     private static int calculateSumOf(Stream<Integer> numbers) {
