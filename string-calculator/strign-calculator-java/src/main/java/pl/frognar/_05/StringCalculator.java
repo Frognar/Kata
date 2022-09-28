a0542ae2fb38306fdeab84a13a29e716678b5a93
Frognar <sebastian.przyszlak@gmail.com>
2022-09-28 05:41:29 +0200

    refactor

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_05/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_05/StringCalculator.java
@@ -7,9 +7,13 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers == null || numbers.isEmpty())
             return 0;
-        var numberList = Arrays.stream(numbers.split(","))
-                .map(Integer::parseInt);
+        var numberList = splitAndConvertNumbers(numbers);
         return calculateSumOf(numberList);
+    }
+
+    private Stream<Integer> splitAndConvertNumbers(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt);
     }
 
     private int calculateSumOf(Stream<Integer> numbers) {
