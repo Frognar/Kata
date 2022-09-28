8b235daf3410c71a28135f74f2d6b3ae0800067c
Frognar <sebastian.przyszlak@gmail.com>
2022-09-28 05:35:04 +0200

    refactor

--- string-calculator/strign-calculator-java/src/test/java/pl/frognar/_05/StringCalculatorTest.java
+++ string-calculator/strign-calculator-java/src/test/java/pl/frognar/_05/StringCalculatorTest.java
@@ -1,18 +1,25 @@
 package pl.frognar._05;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new StringCalculator();
+    }
+
     @Test
     public void addShouldReturnZeroForNull() {
-        var calculator = new StringCalculator();
         assertEquals(0, calculator.add(null));
     }
+
     @Test
     public void addShouldReturnZeroForEmptyString() {
-        var calculator = new StringCalculator();
         assertEquals(0, calculator.add(""));
     }
 }
