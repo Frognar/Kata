b5b550586639a22dd655480efd4a504c8a797194
Frognar <sebastian.przyszlak@gmail.com>
2022-09-28 05:54:57 +0200

    refactor

--- string-calculator/strign-calculator-java/src/test/java/pl/frognar/_05/StringCalculatorTest.java
+++ string-calculator/strign-calculator-java/src/test/java/pl/frognar/_05/StringCalculatorTest.java
@@ -57,9 +57,17 @@ public class StringCalculatorTest {
         assertEquals(expectedValue, calculator.add(numbers));
     }
 
-    @Test
-    public void addShouldThrowExceptionWhenPassedNegativeValues() {
-        Exception exception = assertThrows(NegativeNumbersNotAllowedException.class, () -> calculator.add("-1"));
-        assertEquals(exception.getMessage(), "negatives not allowed: -1");
+    private static Stream<Arguments> negativeNumbersAndExceptionMessage() {
+        return Stream.of(
+                arguments("-1", "negatives not allowed: -1")
+        );
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeNumbersAndExceptionMessage")
+    public void addShouldThrowExceptionWhenPassedNegativeValues(String numbers, String expectedMessage) {
+        Exception exception = assertThrows(NegativeNumbersNotAllowedException.class, () -> calculator.add(numbers));
+        assertEquals(exception.getMessage(), expectedMessage);
     }
 }
