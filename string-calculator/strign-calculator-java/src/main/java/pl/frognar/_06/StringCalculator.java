52c6413b1989e03fc71efef28555cbfdeaedf79d
Frognar <sebastian.przyszlak@gmail.com>
2022-09-29 05:54:09 +0200

    refactor

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_06/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_06/StringCalculator.java
@@ -8,17 +8,22 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers == null || numbers.isEmpty())
             return 0;
-        if (numbers.startsWith("//")) {
-            char customDelimiter = numbers.charAt(2);
-            String delimiter = "%s".formatted(customDelimiter);
-            numbers = numbers.replaceAll(delimiter, ",");
-            numbers = numbers.substring(4);
-        }
+        numbers = replaceCustomDelimiterWithComma(numbers);
         var numberList = splitAndConvertNumbers(numbers);
         assertDoesNotContainsNegativeValues(numberList);
         return calculateSumOf(numberList);
     }
 
+    private static String replaceCustomDelimiterWithComma(String originalNumbers) {
+        if (originalNumbers.startsWith("//")) {
+            char customDelimiter = originalNumbers.charAt(2);
+            String delimiter = "%s".formatted(customDelimiter);
+            String numbers = originalNumbers.replaceAll(delimiter, ",");
+            return numbers.substring(4);
+        }
+
+        return originalNumbers;
+    }
 
     private static final String defaultDelimiters = "[,\n]";
     private List<Integer> splitAndConvertNumbers(String numbers) {
