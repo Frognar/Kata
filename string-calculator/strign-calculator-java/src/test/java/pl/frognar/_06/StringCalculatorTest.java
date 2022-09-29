7ae99fe52222f536addc84d3b06432226947e5a8
Frognar <sebastian.przyszlak@gmail.com>
2022-09-29 05:41:53 +0200

    Init

---
+++ string-calculator/strign-calculator-java/src/test/java/pl/frognar/_06/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package pl.frognar._06;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class StringCalculatorTest {
+    @Test
+    public void shouldFail() {
+        fail();
+    }
+}
