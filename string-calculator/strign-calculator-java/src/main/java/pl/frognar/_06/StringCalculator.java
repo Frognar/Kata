3f96598d4048e279042020e65c2d77b54c032585
Frognar <sebastian.przyszlak@gmail.com>
2022-09-29 05:46:30 +0200

    add "1,2" -> 3

--- string-calculator/strign-calculator-java/src/main/java/pl/frognar/_06/StringCalculator.java
+++ string-calculator/strign-calculator-java/src/main/java/pl/frognar/_06/StringCalculator.java
@@ -1,9 +1,13 @@
 package pl.frognar._06;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers == null || numbers.isEmpty())
             return 0;
-        return Integer.parseInt(numbers);
+        var numberList = Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt);
+        return numberList.reduce(0, Integer::sum);
     }
 }
