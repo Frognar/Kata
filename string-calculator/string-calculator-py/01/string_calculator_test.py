7330c6c0fbc8679bbcc963a1bf48387282231ff2
Frognar <sebastian.przyszlak@gmail.com>
2022-10-01 22:40:13 +0200

    add '2,4,8' -> 14

--- string-calculator/string-calculator-py/01/string_calculator_test.py
+++ string-calculator/string-calculator-py/01/string_calculator_test.py
@@ -21,7 +21,8 @@ class StringCalculatorTestCase(unittest.TestCase):
         self.assertEqual(expected_sum, self.calculator.add(numbers))
 
     @parameterized.expand([
-        ['2,4', 6]
+        ['2,4', 6],
+        ['2,4,8', 14]
     ])
     def test_add_multiple_numbers_separated_by_comma_should_return_sum_of_that_numbers(self, numbers: str, expected_sum: int):
         self.assertEqual(expected_sum, self.calculator.add(numbers))
