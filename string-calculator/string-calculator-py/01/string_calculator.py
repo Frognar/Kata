fff2158fd34bac8d790c0728ae000aebd34e9246
Frognar <sebastian.przyszlak@gmail.com>
2022-10-01 23:15:29 +0200

    add '//[*]\n5*2*6' -> 13

--- string-calculator/string-calculator-py/01/string_calculator.py
+++ string-calculator/string-calculator-py/01/string_calculator.py
@@ -19,9 +19,11 @@ class StringCalculator:
 
     @classmethod
     def replace_custom_delimiters_with_comma(cls, numbers: str) -> str:
-        custom_delimiter_search = search('//.\n', numbers)
+        custom_delimiter_search = search('(//(.)\n)|(//\\[(.)]\n)', numbers)
         if custom_delimiter_search:
-            custom_delimiter = custom_delimiter_search.group()[2]
+            custom_delimiter = custom_delimiter_search.group(2)
+            if not custom_delimiter:
+                custom_delimiter = custom_delimiter_search.group(4)
             numbers = numbers[custom_delimiter_search.end():]
             numbers = numbers.replace(custom_delimiter, ',')
         return numbers
