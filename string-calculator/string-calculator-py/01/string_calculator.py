991f40cf8afe209ae8565d06443914fcc175ceac
Frognar <sebastian.przyszlak@gmail.com>
2022-10-01 22:52:55 +0200

    add '2,-1' -> raise error with -1 in message

--- string-calculator/string-calculator-py/01/string_calculator.py
+++ string-calculator/string-calculator-py/01/string_calculator.py
@@ -1,12 +1,19 @@
 from re import split
 
 
+class NegativesNotAllowedError(RuntimeError):
+    def __int__(self, message):
+        super.__init__(message)
+
+
 class StringCalculator:
     @classmethod
     def add(cls, numbers: str) -> int:
         if not numbers:
             return 0
         number_list = cls.split_and_convert_to_numbers(numbers)
+        if any(n < 0 for n in number_list):
+            raise NegativesNotAllowedError('negatives not allowed: -1')
         return sum(number_list)
 
     default_delimiters = '[,\n]'
--- string-calculator/string-calculator-py/01/string_calculator_test.py
+++ string-calculator/string-calculator-py/01/string_calculator_test.py
@@ -1,5 +1,5 @@
 import unittest
-from string_calculator import StringCalculator
+from string_calculator import StringCalculator, NegativesNotAllowedError
 from parameterized import parameterized
 
 
@@ -27,6 +27,15 @@ class StringCalculatorTestCase(unittest.TestCase):
     ])
     def test_add_multiple_numbers_separated_by_default_delimiter_should_return_sum_of_that_numbers(self, numbers: str, expected_sum: int):
         self.assertEqual(expected_sum, self.calculator.add(numbers))
+
+    @parameterized.expand([
+        ['2,-1', '-1']
+    ])
+    def test_add_some_negatives_numbers_should_raise_error_with_those_numbers_in_message(self, numbers: str, ex_message: str):
+        with self.assertRaises(NegativesNotAllowedError) as error:
+            self.calculator.add(numbers)
+
+        self.assertTrue(ex_message in str(error.exception))
 
 
 if __name__ == '__main__':
