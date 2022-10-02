84e3b49ba22255f07faf7b7c948d724523d457ec
Frognar <sebastian.przyszlak@gmail.com>
2022-10-02 13:01:01 +0200

    refactor

--- string-calculator/string-calculator-py/02/string_calculator.py
+++ string-calculator/string-calculator-py/02/string_calculator.py
@@ -17,19 +17,22 @@ class StringCalculator:
         cls.assert_not_contains_negatives(number_list)
         return cls.calculate_sum_of(number_list)
 
-    @staticmethod
-    def replace_custom_delimiter_with_comma(numbers: str) -> str:
+    @classmethod
+    def replace_custom_delimiter_with_comma(cls, numbers: str) -> str:
         custom_delimiter_search = search('//(.+)\n', numbers)
         if custom_delimiter_search:
-            custom_delimiter_group = custom_delimiter_search.group(1)
-            custom_delimiter_group = custom_delimiter_group.replace('][', ' ')
-            custom_delimiter_group = custom_delimiter_group.replace('[', '')
-            custom_delimiter_group = custom_delimiter_group.replace(']', '')
-            all_custom_delimiters = custom_delimiter_group.split(' ')
+            all_custom_delimiters = cls.find_all_custom_delimiters(custom_delimiter_search.group(1))
             numbers = numbers[custom_delimiter_search.end():]
             for custom_delimiter in all_custom_delimiters:
                 numbers = numbers.replace(custom_delimiter, ',')
         return numbers
+
+    @staticmethod
+    def find_all_custom_delimiters(delimiters_group: str) -> List[str]:
+        delimiters_group = delimiters_group.replace('][', ' ')
+        delimiters_group = delimiters_group.replace('[', '')
+        delimiters_group = delimiters_group.replace(']', '')
+        return delimiters_group.split(' ')
 
     @staticmethod
     def split_and_convert_to_numbers(numbers: str) -> List[int]:
