b800dd9f4035bce00746ffae83051096b156eed8
Frognar <sebastian.przyszlak@gmail.com>
2022-10-02 12:44:10 +0200

    refactor

--- string-calculator/string-calculator-py/02/string_calculator_test.py
+++ string-calculator/string-calculator-py/02/string_calculator_test.py
@@ -3,17 +3,17 @@ from string_calculator import StringCalculator
 
 
 class StringCalculatorTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = StringCalculator()
+
     def test_add_none_should_return_zero(self):
-        calculator = StringCalculator()
-        self.assertEqual(0, calculator.add(None))
+        self.assertEqual(0, self.calculator.add(None))
 
     def test_add_empty_should_return_zero(self):
-        calculator = StringCalculator()
-        self.assertEqual(0, calculator.add(''))
+        self.assertEqual(0, self.calculator.add(''))
 
     def test_add_one_number_in_string_should_return_this_number(self):
-        calculator = StringCalculator()
-        self.assertEqual(4, calculator.add('4'))
+        self.assertEqual(4, self.calculator.add('4'))
 
 
 if __name__ == '__main__':
