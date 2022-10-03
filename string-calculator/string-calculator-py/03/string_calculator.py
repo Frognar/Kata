37a767c0f32a608d1f9489da84d9b6664efd63d6
Frognar <sebastian.przyszlak@gmail.com>
2022-10-03 05:17:18 +0200

    refactor

--- string-calculator/string-calculator-py/03/string_calculator.py
+++ string-calculator/string-calculator-py/03/string_calculator.py
@@ -13,14 +13,18 @@ class StringCalculator:
         if not numbers:
             return 0
         numbers = cls.split_and_convert(numbers)
-        if any(n < 0 for n in numbers):
-            raise NegativesNotAllowedError('negatives not allowed: -1')
+        cls.assert_not_contains_negatives(numbers)
         return cls.calculate_sum_of(numbers)
 
     @staticmethod
     def split_and_convert(numbers: str) -> List[int]:
         default_delimiter = '[,\n]'
         return [int(n) for n in split(default_delimiter, numbers)]
+
+    @staticmethod
+    def assert_not_contains_negatives(numbers: List[int]) -> None:
+        if any(n < 0 for n in numbers):
+            raise NegativesNotAllowedError('negatives not allowed: -1')
 
     @staticmethod
     def calculate_sum_of(numbers: List[int]) -> int:
