bb2faca8eba18822cb58409588be32398da54d90
Frognar <sebastian.przyszlak@gmail.com>
2022-10-03 05:12:28 +0200

    '1,1\n9' -> 11

--- string-calculator/string-calculator-py/03/string_calculator.py
+++ string-calculator/string-calculator-py/03/string_calculator.py
@@ -1,6 +1,10 @@
+from re import split
+
+
 class StringCalculator:
     @classmethod
     def add(cls, numbers: str) -> int:
         if not numbers:
             return 0
-        return sum([int(n) for n in numbers.split(',')])
+        default_delimiter = '[,\n]'
+        return sum([int(n) for n in split(default_delimiter, numbers)])
--- string-calculator/string-calculator-py/03/string_calculator_test.py
+++ string-calculator/string-calculator-py/03/string_calculator_test.py
@@ -21,9 +21,10 @@ class StringCalculatorTestCase(unittest.TestCase):
         self.assertEqual(expected_sum, self.calculator.add(numbers))
 
     @parameterized.expand([
-        ['1,9', 10]
+        ['1,9', 10],
+        ['1,1\n9', 11]
     ])
-    def test_add_multiple_numbers_separated_by_comma_should_return_sum_of_that_numbers(
+    def test_add_multiple_numbers_separated_by_default_delimiter_should_return_sum_of_that_numbers(
             self,
             numbers: str,
             expected_sum: int):
