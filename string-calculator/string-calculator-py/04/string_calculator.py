3db1044fce48f64d6c512bcb41be1332d28da490
Frognar <sebastian.przyszlak@gmail.com>
2022-10-04 05:40:32 +0200

    refactor

--- string-calculator/string-calculator-py/04/string_calculator.py
+++ string-calculator/string-calculator-py/04/string_calculator.py
@@ -6,8 +6,12 @@ class StringCalculator:
     def add(cls, numbers: str) -> int:
         if not numbers:
             return 0
-        numbers = [int(n) for n in numbers.split(',')]
+        numbers = cls.split_and_convert(numbers)
         return cls.calculate_sum_of(numbers)
+
+    @staticmethod
+    def split_and_convert(numbers: str) -> List[int]:
+        return [int(n) for n in numbers.split(',')]
 
     @staticmethod
     def calculate_sum_of(numbers: List[int]) -> int:
