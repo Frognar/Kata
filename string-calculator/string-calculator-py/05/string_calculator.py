9d9cc9cc1d230b537b79ced595e46acc58381bba
Frognar <sebastian.przyszlak@gmail.com>
2022-10-05 05:48:56 +0200

    '1,2' -> 3

--- string-calculator/string-calculator-py/05/string_calculator.py
+++ string-calculator/string-calculator-py/05/string_calculator.py
@@ -6,4 +6,5 @@ class StringCalculator:
     def add(cls, numbers: Optional[str]) -> int:
         if not numbers:
             return 0
-        return int(numbers)
+        numbers = [int(n) for n in numbers.split(',')]
+        return sum(numbers)
