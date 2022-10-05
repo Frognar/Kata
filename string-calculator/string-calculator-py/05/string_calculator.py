f77cb37728aab0d754f159b0dcba4309e477f45d
Frognar <sebastian.przyszlak@gmail.com>
2022-10-05 05:53:33 +0200

    refactor

--- string-calculator/string-calculator-py/05/string_calculator.py
+++ string-calculator/string-calculator-py/05/string_calculator.py
@@ -7,12 +7,17 @@ class StringCalculator:
     def add(cls, numbers: Optional[str]) -> int:
         if not numbers:
             return 0
+        numbers = cls.replace_custom_delimiters_with_comma(numbers)
+        numbers = cls.split_and_convert(numbers)
+        return cls.calculate_sum_of(numbers)
+
+    @classmethod
+    def replace_custom_delimiters_with_comma(cls, numbers: str) -> str:
         if numbers.startswith('//'):
             delimiter = numbers[2]
             numbers = numbers[4:]
             numbers = numbers.replace(delimiter, ',')
-        numbers = cls.split_and_convert(numbers)
-        return cls.calculate_sum_of(numbers)
+        return numbers
 
     @staticmethod
     def split_and_convert(numbers: str) -> List[int]:
