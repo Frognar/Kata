e485b5257f8cb3ae2bf0ed4a9b2809c21dcae171
Frognar <sebastian.przyszlak@gmail.com>
2022-10-08 19:28:27 +0200

    init

---
+++ string-calculator/string-calculator-clj/spec/string_calculator_clj/core_spec.clj
@@ -0,0 +1,9 @@
+(ns string-calculator-clj.core-spec
+  (:require [speclj.core :refer :all]
+            [string-calculator-clj.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
+
+(run-specs)
