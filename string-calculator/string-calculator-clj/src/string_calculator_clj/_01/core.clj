b7a840bdde8d8e985841ed9359b627be751b435d
Frognar <sebastian.przyszlak@gmail.com>
2022-10-08 19:40:49 +0200

    refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_01/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_01/core.clj
@@ -1,8 +1,12 @@
 (ns string-calculator-clj._01.core
   (:require [clojure.string :as str]))
 
+(defn split-and-convert [numbers]
+  (map #(Integer/parseInt %)
+       (str/split numbers #",")))
+
 (defn add [numbers]
   (if (or (nil? numbers) (empty? numbers))
     0
-      (reduce + (map #(Integer/parseInt %) (str/split numbers #","))))
+      (reduce + (split-and-convert numbers)))
     )
