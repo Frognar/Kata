358c1a83d58d6e2b3bca033c4c78b7c29be63820
Frognar <sebastian.przyszlak@gmail.com>
2022-10-08 22:54:50 +0200

    refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_01/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_01/core.clj
@@ -13,18 +13,19 @@
     (re-pattern (str/join #"|" (sort-by count #(compare %2 %1) (str/split (nth match 2) #"\Q][\E"))))
     (second match)))
 
+(defn replace-all-custom-delimiters-with-comma [numbers match]
+  (let [[numbers custom_delimiter] [(subs numbers (count (first match))) (get-all-custom-delimiters match)]]
+    (str/replace numbers custom_delimiter ",")))
+
 (defn replace-custom-delimiter-with-comma [numbers]
   (let [custom-delimiter-pattern #"//(.)\n|//\[(.+)]\n"
         matcher (re-matcher custom-delimiter-pattern numbers)
         match (re-find matcher)]
     (if (nil? match)
       numbers
-      (let [[numbers custom_delimiter] [(subs numbers (count (first match))) (get-all-custom-delimiters match)]]
-        (str/replace numbers custom_delimiter ",")
-        ))))
+      (replace-all-custom-delimiters-with-comma numbers match))))
 
 (defn add [numbers]
   (if (empty? numbers)
     0
-    (sum-of (split-and-convert (replace-custom-delimiter-with-comma numbers))))
-  )
+    (sum-of (split-and-convert (replace-custom-delimiter-with-comma numbers)))))
