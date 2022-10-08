b7ea48575ea78aed2c734ba1d155bad87df4a0f8
Frognar <sebastian.przyszlak@gmail.com>
2022-10-08 20:18:05 +0200

    refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_01/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_01/core.clj
@@ -8,11 +8,16 @@
 (defn sum-of [numbers]
   (reduce + numbers))
 
+(defn replace-custom-delimiter-with-comma [numbers]
+  (if (str/starts-with? numbers "//")
+    (let [comma ","
+          delimiter (str (nth numbers 2))
+          rest_numbers (subs numbers 4)]
+      (str/replace rest_numbers delimiter comma))
+    numbers))
+
 (defn add [numbers]
   (if (or (nil? numbers) (empty? numbers))
     0
-    (let [n (if (str/starts-with? numbers "//")
-              (str/replace (subs numbers 4 (count numbers)) (str (nth numbers 2)) ",")
-              numbers)]
-      (sum-of (split-and-convert n))))
+    (sum-of (split-and-convert (replace-custom-delimiter-with-comma numbers))))
   )
