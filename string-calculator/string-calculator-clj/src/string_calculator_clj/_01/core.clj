64fb6d13dfb91fe52dc47a1f0c5c667ac6b185ef
Frognar <sebastian.przyszlak@gmail.com>
2022-10-08 22:49:58 +0200

    '//[>][>>]\n1>2<3' -> 6

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_01/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_01/core.clj
@@ -18,7 +18,7 @@
             numbers (subs numbers (count (first match)))
             custom_delimiter (if
                                (nil? (second match))
-                               (re-pattern (str/join #"|" (str/split (nth match 2) #"\Q][\E")))
+                               (re-pattern (str/join #"|" (sort-by count #(compare %2 %1) (str/split (nth match 2) #"\Q][\E"))))
                                (second match))]
         (str/replace numbers custom_delimiter comma)
         ))))
