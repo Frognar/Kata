db237579cad2d1ce66971c0bb6b34b766477b6d2
Frognar <sebastian.przyszlak@gmail.com>
2022-10-09 08:45:46 +0200

    recover missing files

---
+++ bowling-game/bowling-game-clj/spec/bowling_game_clj/_03/core_spec.clj
@@ -0,0 +1,27 @@
+(ns bowling-game-clj._03.core-spec
+  (:require [speclj.core :refer :all]
+            [bowling-game-clj._03.core :refer :all]))
+
+(describe "The Bowling Game"
+          (it "score 0 for all zeros"
+              (should= 0 (score (repeat 20 0))))
+          (it "score 20 for all ones"
+              (should= 20 (score (repeat 20 1))))
+          (it "score spare"
+              (should= 18 (score [5 5 4 0])))
+          (it "score strike"
+              (should= 28 (score [10 5 4])))
+          (it "score all strikes"
+              (should= 300 (score (repeat 12 10))))
+          )
+
+(describe "Frame partitioning"
+          (it "partition open frames"
+              (should= [[1 2] [3 4]] (->frames [1 2 3 4])))
+          (it "partition spare frames"
+              (should= [[5 5 3] [3 4]] (->frames [5 5 3 4])))
+          (it "partition strike frames"
+              (should= [[10 5 3] [5 3]] (->frames [10 5 3])))
+          )
+
+(run-specs)
