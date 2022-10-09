2240f5791ffedaa366bcc3dae906337b5cc24d5a
Frognar <sebastian.przyszlak@gmail.com>
2022-10-09 08:02:47 +0200

    '2,3' -> 5 refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_02/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_02/core.clj
@@ -1,7 +1,10 @@
 (ns string-calculator-clj._02.core
   (:require [clojure.string :as str]))
 
+(defn sum-of [numbers]
+  (reduce + numbers))
+
 (defn add [numbers]
   (if (empty? numbers)
     0
-    (reduce + (map #(Integer/parseInt %) (str/split numbers #",")))))
+    (sum-of (map #(Integer/parseInt %) (str/split numbers #",")))))
