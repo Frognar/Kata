4f1b020d73859a2e766e24a26031e19f152da78f
Frognar <sebastian.przyszlak@gmail.com>
2022-10-10 05:12:59 +0200

    nil -> 0

--- string-calculator/string-calculator-clj/spec/string_calculator_clj/_03/core_spec.clj
+++ string-calculator/string-calculator-clj/spec/string_calculator_clj/_03/core_spec.clj
@@ -1,9 +1,10 @@
 (ns string-calculator-clj._03.core-spec
-  (:require [speclj.core :refer :all]))
+  (:require [speclj.core :refer :all]
+            [string-calculator-clj._03.core :refer :all]))
 
 (describe "String Calculator"
-          (it "Fail"
-              (should= 0 1))
+          (it "Nil -> 0"
+              (should= 0 (add nil)))
           )
 
 (run-specs)
--- string-calculator/string-calculator-clj/src/string_calculator_clj/_03/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_03/core.clj
@@ -1,1 +1,4 @@
 (ns string-calculator-clj._03.core)
+
+(defn add [numbers]
+  0)
