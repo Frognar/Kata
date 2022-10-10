30b7b8dc2c196ed36d07771fe917e2f0fe75c36d
Frognar <sebastian.przyszlak@gmail.com>
2022-10-10 05:25:23 +0200

    '//;\n15;16' -> 31 refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_03/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_03/core.clj
@@ -8,10 +8,13 @@
   (let [numbers (str/split numbers #"[,\n]")]
     (map #(Integer/parseInt %) numbers)))
 
+(defn replace-custom-delimiter-with-comma [numbers]
+  (if (str/starts-with? numbers "//")
+    (str/replace (subs numbers 4) (str (nth numbers 2)) ",")
+    numbers))
+
 (defn add [numbers]
   (if (empty? numbers)
     0
-    (let [numbers (split-and-convert (if (str/starts-with? numbers "//")
-                                       (str/replace (subs numbers 4) (str (nth numbers 2)) ",")
-                                       numbers))]
+    (let [numbers (split-and-convert (replace-custom-delimiter-with-comma numbers))]
       (sum-of numbers))))
