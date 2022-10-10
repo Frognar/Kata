af0729a14fa351cd6c3ef5ac59a4a68d07b4306b
Frognar <sebastian.przyszlak@gmail.com>
2022-10-10 05:35:26 +0200

    '//[;][;;]\n22;23;;24' -> 69

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_03/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_03/core.clj
@@ -10,7 +10,7 @@
 
 (defn find-all-delimiters [match]
   (if (nil? (second match))
-    (re-pattern (str/join "|" (str/split (nth match 2) #"]\Q[\E")))
+    (re-pattern (str/join "|" (sort-by count #(compare %2 %1) (str/split (nth match 2) #"]\Q[\E"))))
     (second match)))
 
 (defn replace-custom-delimiter-with-comma [numbers]
