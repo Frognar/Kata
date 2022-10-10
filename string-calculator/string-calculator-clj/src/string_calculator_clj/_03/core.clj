2a48e13b2d6fda75350eb6becca5240de0d4369c
Frognar <sebastian.przyszlak@gmail.com>
2022-10-10 05:17:41 +0200

    '11,12' -> 23 refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_03/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_03/core.clj
@@ -4,8 +4,11 @@
 (defn sum-of [numbers]
   (reduce + numbers))
 
+(defn split [numbers]
+  (str/split numbers #","))
+
 (defn add [numbers]
   (if (empty? numbers)
     0
-    (let [numbers (str/split numbers #",")]
+    (let [numbers (split numbers)]
       (sum-of (map #(Integer/parseInt %) numbers)))))
