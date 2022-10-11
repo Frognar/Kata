ca48ec260a9e67ac551852986b09d464b11ccf28
Frognar <sebastian.przyszlak@gmail.com>
2022-10-11 20:22:38 +0200

    '-6' -> throw exception refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_04/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_04/core.clj
@@ -1,14 +1,16 @@
 (ns string-calculator-clj._04.core
   (:require [clojure.string :as str]))
 
+(defn sum-of [numbers] (reduce + numbers))
+
+(defn contains-negative? [numbers] (some #(< % 0) numbers))
+
 (defn split-and-convert [numbers]
   (map #(Integer/parseInt %) (str/split numbers #"[,\n]")))
-
-(defn sum-of [numbers] (reduce + numbers))
 
 (defn add [numbers]
   (if (empty? numbers)
     0
     (let [numbers (split-and-convert numbers)]
-      (if (some #(< % 0) numbers) (throw IllegalArgumentException))
+      (if (contains-negative? numbers) (throw IllegalArgumentException))
       (sum-of numbers))))
