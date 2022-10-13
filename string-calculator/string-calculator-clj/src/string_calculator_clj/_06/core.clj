ddf373712ed5a5c0c17f1ffb2e7c92607a93a45b
Frognar <sebastian.przyszlak@gmail.com>
2022-10-13 09:22:45 +0200

    '//>\n64>128' -> 192 refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_06/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_06/core.clj
@@ -8,11 +8,14 @@
   (let [contains-negative? (some #(< % 0) numbers)]
     (if contains-negative? (throw IllegalArgumentException))))
 
+(defn replace-custom-delimiter-with-comma [numbers]
+  (if (not (str/starts-with? numbers "//"))
+    numbers
+    (str/replace (subs numbers 4) (str (nth numbers 2)) ",")))
+
 (defn split-and-convert-to-integers [numbers]
   (let [default-delimiters #"[,\n]"
-        numbers (if (not (str/starts-with? numbers "//"))
-                  numbers
-                  (str/replace (subs numbers 4) (str (nth numbers 2)) ","))]
+        numbers (replace-custom-delimiter-with-comma numbers)]
     (letfn [(split-on-default-delimiter [numbers] (str/split numbers default-delimiters))
             (convert-to-integers [numbers] (map #(Integer/parseInt %) numbers))]
       (convert-to-integers (split-on-default-delimiter numbers)))))
