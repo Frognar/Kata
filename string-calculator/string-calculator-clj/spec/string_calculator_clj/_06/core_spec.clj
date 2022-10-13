767ba05bce5868ca3cc769a4abb1c29161fb88d9
Frognar <sebastian.przyszlak@gmail.com>
2022-10-13 09:04:06 +0200

    '' -> 0

--- string-calculator/string-calculator-clj/spec/string_calculator_clj/_06/core_spec.clj
+++ string-calculator/string-calculator-clj/spec/string_calculator_clj/_06/core_spec.clj
@@ -4,6 +4,9 @@
 
 (describe "String Calculator"
           (it "nil -> 0"
-              (should= 0 (add nil))))
+              (should= 0 (add nil)))
+          (it "'' -> 0"
+              (should= 0 (add "")))
+          )
 
 (run-specs)
