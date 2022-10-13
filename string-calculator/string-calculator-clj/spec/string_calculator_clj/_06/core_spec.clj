4f64052f6fc61d358eac025409ebbef6fb3892de
Frognar <sebastian.przyszlak@gmail.com>
2022-10-13 09:06:19 +0200

    '2,4' -> 6

--- string-calculator/string-calculator-clj/spec/string_calculator_clj/_06/core_spec.clj
+++ string-calculator/string-calculator-clj/spec/string_calculator_clj/_06/core_spec.clj
@@ -9,6 +9,8 @@
               (should= 0 (add "")))
           (it "'1' -> 1"
               (should= 1 (add "1")))
+          (it "'2,4' -> 6"
+              (should= 6 (add "2,4")))
           )
 
 (run-specs)
--- string-calculator/string-calculator-clj/src/string_calculator_clj/_06/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_06/core.clj
@@ -1,6 +1,7 @@
-(ns string-calculator-clj._06.core)
+(ns string-calculator-clj._06.core
+  (:require [clojure.string :as str]))
 
 (defn add [numbers]
   (if (empty? numbers)
     0
-    (Integer/parseInt numbers)))
+    (reduce + (map #(Integer/parseInt %) (str/split numbers #",")))))
