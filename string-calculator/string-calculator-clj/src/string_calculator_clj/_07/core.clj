d2aa16aba86ff887077ac43dd6ed38bf9a51ad5f
Frognar <sebastian.przyszlak@gmail.com>
2022-10-14 05:34:07 +0200

    '1' -> 1 refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_07/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_07/core.clj
@@ -1,6 +1,8 @@
 (ns string-calculator-clj._07.core)
 
+(defn convert-to-integer [number] (Integer/parseInt number))
+
 (defn add [numbers]
   (if (empty? numbers)
     0
-    (Integer/parseInt numbers)))
+    (convert-to-integer numbers)))
