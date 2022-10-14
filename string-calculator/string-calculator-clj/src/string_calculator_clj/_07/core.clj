a77edee16cc6fb8ed2c190b3747ff11803875334
Frognar <sebastian.przyszlak@gmail.com>
2022-10-14 05:35:43 +0200

    '3,5' -> 8 refactor

--- string-calculator/string-calculator-clj/src/string_calculator_clj/_07/core.clj
+++ string-calculator/string-calculator-clj/src/string_calculator_clj/_07/core.clj
@@ -3,7 +3,9 @@
 
 (defn convert-to-integer [number] (Integer/parseInt number))
 
+(defn split-on-comma [numbers] (str/split numbers #","))
+
 (defn add [numbers]
   (if (empty? numbers)
     0
-    (reduce + (map #(convert-to-integer %) (str/split numbers #",")))))
+    (reduce + (map #(convert-to-integer %) (split-on-comma numbers)))))
