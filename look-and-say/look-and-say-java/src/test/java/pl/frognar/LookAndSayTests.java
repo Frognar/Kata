790a7d616237431769cb2073ca32637ae8e551c0
Frognar <sebastian.przyszlak@gmail.com>
2022-10-15 12:26:50 +0200

    init

---
+++ look-and-say/look-and-say-java/src/test/java/pl/frognar/LookAndSayTests.java
@@ -0,0 +1,12 @@
+package pl.frognar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class LookAndSayTests {
+    @Test
+    void Fail() {
+        fail();
+    }
+}
