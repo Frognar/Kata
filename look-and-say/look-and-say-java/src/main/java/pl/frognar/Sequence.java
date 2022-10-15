721f03a054465eb78b30160750c9dee7f7f7a290
Frognar <sebastian.przyszlak@gmail.com>
2022-10-15 12:34:22 +0200

    "2" -> "12"

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/Sequence.java
@@ -1,7 +1,7 @@
 package pl.frognar;
 
 public class Sequence {
-    public static String lookAndSay(String inout) {
-        return "11";
+    public static String lookAndSay(String input) {
+        return "1%s".formatted(input);
     }
 }
--- look-and-say/look-and-say-java/src/test/java/pl/frognar/LookAndSayTests.java
+++ look-and-say/look-and-say-java/src/test/java/pl/frognar/LookAndSayTests.java
@@ -7,7 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LookAndSayTests {
     @ParameterizedTest
-    @CsvSource({"1,11"})
+    @CsvSource({"1,11",
+                "2,12"})
     void oneDigit(String input, String expected) {
         assertEquals(expected, Sequence.lookAndSay(input));
     }
