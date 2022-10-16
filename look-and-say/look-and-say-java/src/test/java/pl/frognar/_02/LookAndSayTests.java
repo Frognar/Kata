c3156b55354d83ff1e390d123c0f9fbf251afa94
Frognar <sebastian.przyszlak@gmail.com>
2022-10-16 10:33:58 +0200

    "2" -> "12"

--- look-and-say/look-and-say-java/src/test/java/pl/frognar/_02/LookAndSayTests.java
+++ look-and-say/look-and-say-java/src/test/java/pl/frognar/_02/LookAndSayTests.java
@@ -1,12 +1,15 @@
 package pl.frognar._02;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LookAndSayTests {
-    @Test
-    void nextSequenceElementTest() {
-        assertEquals("11", Sequence.lookAndSay("1"));
+    @ParameterizedTest
+    @CsvSource({"1,11",
+                "2,12"})
+    void nextSequenceElementTest(String input, String expected) {
+        assertEquals(expected, Sequence.lookAndSay(input));
     }
 }
