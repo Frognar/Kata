4f96f52c233a8c4e4eb3b4443b673323d0a7e90b
Frognar <sebastian.przyszlak@gmail.com>
2022-10-17 05:23:36 +0200

    "12" -> "1112"

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/_03/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/_03/Sequence.java
@@ -2,9 +2,12 @@ package pl.frognar._03;
 
 public class Sequence {
     public static String lookAndSay(String input) {
-        if (input.length() == 2)
-            return "2%s".formatted(input.charAt(0));
-        else
+        if (input.length() == 2) {
+            if (input.charAt(0) == input.charAt(1))
+                return "2%s".formatted(input.charAt(0));
+            else
+                return "1112";
+        } else
             return "1%s".formatted(input);
     }
 }
