5ec1f8ca081546611af4629e81a051631dab5927
Frognar <sebastian.przyszlak@gmail.com>
2022-10-17 05:29:20 +0200

    refactor

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/_03/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/_03/Sequence.java
@@ -5,10 +5,11 @@ public class Sequence {
         String result = "";
         int count = 1;
         if (input.length() == 2) {
-            if (input.charAt(0) == input.charAt(1)) {
+            char currentDigit = input.charAt(0);
+            if (currentDigit == input.charAt(1)) {
                 count++;
             } else {
-                result += "%s%s".formatted(count, input.charAt(0));
+                result += "%s%s".formatted(count, currentDigit);
             }
         }
 
