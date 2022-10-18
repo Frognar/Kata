0a057feb0474ca69c0ceff1fc829d148d6792bf3
Frognar <sebastian.przyszlak@gmail.com>
2022-10-18 13:00:25 +0200

    refactor tests

--- look-and-say/look-and-say-java/src/test/java/pl/frognar/_04/LookAndSayTests.java
+++ look-and-say/look-and-say-java/src/test/java/pl/frognar/_04/LookAndSayTests.java
@@ -1,12 +1,16 @@
 package pl.frognar._04;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LookAndSayTests {
-    @Test
-    void nextSequenceElementTest() {
-        assertEquals("11", Sequence.lookAndSay("1"));
+    @ParameterizedTest
+    @CsvSource({
+            "1,11",
+    })
+    void nextSequenceElementTest(String input, String expected) {
+        assertEquals(expected, Sequence.lookAndSay(input));
     }
 }
