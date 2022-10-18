c7cf522198a7bc033004d66de8f7611274ab440d
Frognar <sebastian.przyszlak@gmail.com>
2022-10-18 13:02:36 +0200

    refactor

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/_04/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/_04/Sequence.java
@@ -4,6 +4,6 @@ public class Sequence {
     public static String lookAndSay(String input) {
         if (input.length() == 2)
             return "2%s".formatted(input.charAt(0));
-        return "1%s".formatted(input);
+        return "1%s".formatted(input.charAt(input.length() - 1));
     }
 }
