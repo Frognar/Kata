a757a1fc1a2fc352181a8495597a41bc76900dd2
Frognar <sebastian.przyszlak@gmail.com>
2022-10-19 05:42:03 +0200

    "11" -> "21"

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/_05/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/_05/Sequence.java
@@ -2,6 +2,8 @@ package pl.frognar._05;
 
 public class Sequence {
     public static String lookAndSay(String input) {
+        if (input.length() == 2)
+            return "2%s".formatted(input.charAt(0));
         return "1%s".formatted(input);
     }
 }
