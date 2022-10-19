0a07ce5516296f3656102bca5ca6db699f937041
Frognar <sebastian.przyszlak@gmail.com>
2022-10-19 05:46:01 +0200

    refactor

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/_05/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/_05/Sequence.java
@@ -11,8 +11,9 @@ public class Sequence {
                 return result;
             }
             else {
-                result += "1%s".formatted(input.charAt(0));
-                result += "1%s".formatted(input.charAt(1));
+                int count = 1;
+                result += "%s%s".formatted(count, input.charAt(0));
+                result += "%s%s".formatted(count, input.charAt(1));
                 return result;
             }
         }
