a2389531bf0a0dc45d3902035f1008cf9b57a663
Frognar <sebastian.przyszlak@gmail.com>
2022-10-21 05:41:32 +0200

    "21" -> "1211" refactor

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/_07/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/_07/Sequence.java
@@ -3,19 +3,17 @@ package pl.frognar._07;
 public class Sequence {
     public static String lookAndSay(String input) {
         String result = "";
+        int count = 1;
         if (input.length() == 2) {
             if (input.charAt(0) == input.charAt(1)) {
-                int count = 1;
                 count++;
                 result += "%s%s".formatted(count, input.charAt(0));
                 return result;
             }
-            int count = 1;
             result += "%s%s".formatted(count, input.charAt(0));
             result += "%s%s".formatted(count, input.charAt(1));
             return result;
         }
-        int count = 1;
         result += "%s%s".formatted(count, input);
         return result;
     }
