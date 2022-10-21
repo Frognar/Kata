848701e94cd11cdc981f283eb235411a1d750635
Frognar <sebastian.przyszlak@gmail.com>
2022-10-21 05:45:49 +0200

    "1211" -> "111221"

--- look-and-say/look-and-say-java/src/test/java/pl/frognar/_07/LookAndSayTests.java
+++ look-and-say/look-and-say-java/src/test/java/pl/frognar/_07/LookAndSayTests.java
@@ -10,7 +10,8 @@ public class LookAndSayTests {
     @CsvSource({
             "1,11",
             "11,21",
-            "21,1211"
+            "21,1211",
+            "1211,111221"
     })
     void nextSequenceElementTest(String input, String expected) {
         assertEquals(expected, Sequence.lookAndSay(input));
