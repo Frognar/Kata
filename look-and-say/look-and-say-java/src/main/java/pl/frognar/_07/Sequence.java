0c764bb25a2e30acb7cfcd21cdc54e3434fb5ab4
Frognar <sebastian.przyszlak@gmail.com>
2022-10-21 05:46:03 +0200

    "1211" -> "111221" refactor

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/_07/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/_07/Sequence.java
@@ -5,14 +5,12 @@ public class Sequence {
         String result = "";
         int count = 1;
         for (int index = 0; index < input.length() - 1; index++) {
-            if (input.length() > 1) {
-                char currentDigit = input.charAt(index);
-                char nextDigit = input.charAt(index + 1);
-                if (currentDigit == nextDigit) {
-                    count++;
-                } else {
-                    result += say(count, currentDigit);
-                }
+            char currentDigit = input.charAt(index);
+            char nextDigit = input.charAt(index + 1);
+            if (currentDigit == nextDigit) {
+                count++;
+            } else {
+                result += say(count, currentDigit);
             }
         }
         char lastDigit = input.charAt(input.length() - 1);
