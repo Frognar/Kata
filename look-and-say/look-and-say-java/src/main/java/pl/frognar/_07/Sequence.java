64f4f6612e5b9071ec1d04a85d1691accf20653b
Frognar <sebastian.przyszlak@gmail.com>
2022-10-21 05:38:54 +0200

    "21" -> "1211" refactor

--- look-and-say/look-and-say-java/src/main/java/pl/frognar/_07/Sequence.java
+++ look-and-say/look-and-say-java/src/main/java/pl/frognar/_07/Sequence.java
@@ -8,7 +8,9 @@ public class Sequence {
                 result += "2%s".formatted(input.charAt(0));
                 return result;
             }
-            return "1%s1%s".formatted(input.charAt(0), input.charAt(1));
+            String result = "";
+            result += "1%s1%s".formatted(input.charAt(0), input.charAt(1));
+            return result;
         }
         return "1%s".formatted(input);
     }
