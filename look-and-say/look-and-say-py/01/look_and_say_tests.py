1a37f6d836c0195edcc0ef23dd778a17fa6c1b98
Frognar <sebastian.przyszlak@gmail.com>
2022-10-22 10:05:24 +0200

    "1" -> "11" refactor tests

--- look-and-say/look-and-say-py/01/look_and_say_tests.py
+++ look-and-say/look-and-say-py/01/look_and_say_tests.py
@@ -1,10 +1,14 @@
 import unittest
+from parameterized import parameterized
 from sequence import look_and_say
 
 
 class LookAndSayTestCase(unittest.TestCase):
-    def test_look_and_say(self):
-        self.assertEqual("11", look_and_say("1"))
+    @parameterized.expand([
+        ("1", "11"),
+    ])
+    def test_look_and_say(self, current: str, expected: str):
+        self.assertEqual(expected, look_and_say(current))
 
 
 if __name__ == '__main__':
