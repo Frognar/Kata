20b1d3770baaa08e98494ed43aa35f5dd402e9ed
Frognar <sebastian.przyszlak@gmail.com>
2022-10-23 08:27:43 +0200

    "1", 2 -> "1 11 21"

--- look-and-say/look-and-say-py/02/look_and_say_tests.py
+++ look-and-say/look-and-say-py/02/look_and_say_tests.py
@@ -17,6 +17,7 @@ class LookAndSayTestCase(unittest.TestCase):
     @parameterized.expand([
         ("1", 0, "1"),
         ("1", 1, "1 11"),
+        ("1", 2, "1 11 21"),
     ])
     def test_look_and_say_sequence(self, start: str, depth: int, expected: str):
         self.assertEqual(expected, look_and_say_sequence(start, depth))
