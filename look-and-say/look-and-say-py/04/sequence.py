9f6ecce891400c68e5f8fe3a03f82e25bab7af28
Frognar <sebastian.przyszlak@gmail.com>
2022-10-25 15:24:21 +0200

    "1211" -> "111221" refactor

--- look-and-say/look-and-say-py/04/sequence.py
+++ look-and-say/look-and-say-py/04/sequence.py
@@ -2,10 +2,9 @@ def look_and_say(input_value: str) -> str:
     result = ''
     count = 1
     for index in range(len(input_value) - 1):
-        if len(input_value) > 1:
-            if input_value[index] == input_value[index + 1]:
-                count += 1
-            else:
-                result += f'{count}{input_value[index]}'
+        if input_value[index] == input_value[index + 1]:
+            count += 1
+        else:
+            result += f'{count}{input_value[index]}'
     result += f'{count}{input_value[-1]}'
     return result
