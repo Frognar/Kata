742a64a1322fe0d2330c880cbaa5ceaf578e4387
Frognar <sebastian.przyszlak@gmail.com>
2022-10-25 15:23:25 +0200

    "21" -> "1211" refactor

--- look-and-say/look-and-say-py/04/sequence.py
+++ look-and-say/look-and-say-py/04/sequence.py
@@ -2,7 +2,7 @@ def look_and_say(input_value: str) -> str:
     result = ''
     count = 1
     index = 0
-    if len(input_value) == 2:
+    if len(input_value) > 1:
         if input_value[index] == input_value[index + 1]:
             count += 1
         else:
