fbffffefe76662aab58163f0e921923cce0228cb
Frognar <sebastian.przyszlak@gmail.com>
2022-10-26 06:03:06 +0200

    '3', 1 -> '3 13' refactor

--- look-and-say/look-and-say-py/05/sequence.py
+++ look-and-say/look-and-say-py/05/sequence.py
@@ -20,7 +20,8 @@ def say(count: int, digit: str) -> str:
 
 def look_and_say_sequence(start: str, depth: int) -> str:
     result = start
+    current = start
     if depth > 0:
-        current = look_and_say(start)
+        current = look_and_say(current)
         result += f' {current}'
     return result
