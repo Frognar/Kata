a34b1502000ee6304ff248d61b084097c94aa1c2
Frognar <sebastian.przyszlak@gmail.com>
2022-10-26 06:01:21 +0200

    '3', 0 -> '3'

--- look-and-say/look-and-say-py/05/sequence.py
+++ look-and-say/look-and-say-py/05/sequence.py
@@ -16,3 +16,7 @@ def look_and_say(input_value: str) -> str:
 
 def say(count: int, digit: str) -> str:
     return f'{count}{digit}'
+
+
+def look_and_say_sequence(start: str, depth: int) -> str:
+    return '3'
