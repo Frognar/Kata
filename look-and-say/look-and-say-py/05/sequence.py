7d2972e7c2c2b186f92da5f4cc50496cffb014ab
Frognar <sebastian.przyszlak@gmail.com>
2022-10-26 05:51:47 +0200

    '21' -> '1211'

--- look-and-say/look-and-say-py/05/sequence.py
+++ look-and-say/look-and-say-py/05/sequence.py
@@ -1,5 +1,8 @@
 def look_and_say(input_value: str) -> str:
     count = 1
     if len(input_value) == 2:
-        count += 1
+        if input_value[0] == input_value[1]:
+            count += 1
+        else:
+            return f'{count}{input_value[0]}{count}{input_value[1]}'
     return f'{count}{input_value[-1]}'
