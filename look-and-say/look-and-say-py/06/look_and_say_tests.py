2a7cf9ebec3b7b2e553dfb65885369cd7bec1eb1
Frognar <sebastian.przyszlak@gmail.com>
2022-10-27 14:59:13 +0200

    '22' -> '22'

--- look-and-say/look-and-say-py/06/look_and_say_tests.py
+++ look-and-say/look-and-say-py/06/look_and_say_tests.py
@@ -7,6 +7,7 @@ class LookAndSayTestCase(unittest.TestCase):
     @parameterized.expand([
         ('2', '12'),
         ('12', '1112'),
+        ('22', '22')
     ])
     def test_look_and_say(self, current: str, expected: str):
         self.assertEqual(expected, look_and_say(current))
--- look-and-say/look-and-say-py/06/sequence.py
+++ look-and-say/look-and-say-py/06/sequence.py
@@ -2,6 +2,9 @@ def look_and_say(input_value: str) -> str:
     result = ''
     count = 1
     if len(input_value) == 2:
-        result += f'{count}{input_value[0]}'
+        if input_value[0] == input_value[1]:
+            count += 1
+        else:
+            result += f'{count}{input_value[0]}'
     result += f'{count}{input_value[-1]}'
     return result
