0c45327f349af6f9b44ea883bc6fc9acc1e68352
Frognar <sebastian.przyszlak@gmail.com>
2022-10-27 14:54:53 +0200

    '12' -> '1112'

--- look-and-say/look-and-say-py/06/look_and_say_tests.py
+++ look-and-say/look-and-say-py/06/look_and_say_tests.py
@@ -6,6 +6,7 @@ from sequence import look_and_say
 class LookAndSayTestCase(unittest.TestCase):
     @parameterized.expand([
         ('2', '12'),
+        ('12', '1112'),
     ])
     def test_look_and_say(self, current: str, expected: str):
         self.assertEqual(expected, look_and_say(current))
