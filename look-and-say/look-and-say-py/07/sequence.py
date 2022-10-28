65bf1d276ada8bc3720c8e13d1c67fb9b79fe590
Frognar <sebastian.przyszlak@gmail.com>
2022-10-28 05:43:23 +0200

    '11' -> '21' refactor

--- look-and-say/look-and-say-py/07/sequence.py
+++ look-and-say/look-and-say-py/07/sequence.py
@@ -1,4 +1,5 @@
 def look_and_say(input_value: str) -> str:
     if len(input_value) == 2:
-        return f'2{input_value[0]}'
+        result = f'2{input_value[0]}'
+        return result
     return f'1{input_value}'
