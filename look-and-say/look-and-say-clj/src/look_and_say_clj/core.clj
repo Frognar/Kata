cc167003ade99bb51fc87f9793e30436cbde5651
Frognar <sebastian.przyszlak@gmail.com>
2022-10-29 10:50:53 +0200

    '21' -> '1211' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/core.clj
@@ -2,8 +2,9 @@
 
 (defn look-and-say [input]
   (if (> (count input) 1)
-    (let [current_digit (nth input 0)]
-      (if (= current_digit (nth input 1))
+    (let [current_digit (nth input 0)
+          next_digit (nth input 1)]
+      (if (= current_digit next_digit)
         (str 2 current_digit)
         (str 1 current_digit 1 (nth input 1))))
     (str 1 input)))
