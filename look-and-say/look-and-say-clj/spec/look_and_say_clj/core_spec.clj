69459129d7fe77b6660cdcd86fd18cf3f2ebc22d
Frognar <sebastian.przyszlak@gmail.com>
2022-10-29 10:44:04 +0200

    '1' -> '11'

--- look-and-say/look-and-say-clj/spec/look_and_say_clj/core_spec.clj
+++ look-and-say/look-and-say-clj/spec/look_and_say_clj/core_spec.clj
@@ -2,9 +2,9 @@
   (:require [speclj.core :refer :all]
             [look-and-say-clj.core :refer :all]))
 
-(describe "a test"
-          (it "FIXME, I fail."
-              (should= 0 1))
+(describe "Look and say"
+          (it "'1' -> '11'"
+              (should= "11" (look-and-say "1")))
           )
 
 (run-specs)
--- look-and-say/look-and-say-clj/src/look_and_say_clj/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/core.clj
@@ -1,2 +1,4 @@
 (ns look-and-say-clj.core)
 
+(defn look-and-say [input]
+  "11")
