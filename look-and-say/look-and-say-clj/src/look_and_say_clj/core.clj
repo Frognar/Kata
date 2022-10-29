d1fdb371b682d0cdc3fbe15b203613fec6125d3a
Frognar <sebastian.przyszlak@gmail.com>
2022-10-29 11:32:59 +0200

    '1', 1 -> '1:11' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/core.clj
@@ -5,12 +5,15 @@
          input input
          dig_count 1]
     (if (> (count input) 1)
-        (if (= (first input) (second input))
-          (recur res (subs input 1) (inc dig_count))
-          (recur (conj res (str dig_count (first input))) (subs input 1) 1))
+      (if (= (first input) (second input))
+        (recur res (subs input 1) (inc dig_count))
+        (recur (conj res (str dig_count (first input))) (subs input 1) 1))
       (str (apply str res) dig_count input))))
 
 (defn look-and-say-sequence [input depth]
-  (if (> depth 0)
-    (str input ":" (look-and-say input))
-    input))
+  (loop [res [input]
+         current input
+         depth depth]
+    (if (> depth 0)
+      (str (apply str res) ":" (look-and-say current))
+      input)))
