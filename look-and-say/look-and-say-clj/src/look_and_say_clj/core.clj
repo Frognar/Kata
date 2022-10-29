cc2fb0ff807db3b673bdcb01d292c7e2d5409de3
Frognar <sebastian.przyszlak@gmail.com>
2022-10-29 11:23:05 +0200

    '21' -> '1211' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/core.clj
@@ -1,10 +1,11 @@
 (ns look-and-say-clj.core)
 
 (defn look-and-say [input]
-  (loop [input input
+  (loop [res []
+         input input
          dig_count 1]
     (if (> (count input) 1)
         (if (= (first input) (second input))
-          (recur (subs input 1) (inc dig_count))
+          (recur res (subs input 1) (inc dig_count))
           (str dig_count (first input) dig_count (second input)))
       (str dig_count input))))
