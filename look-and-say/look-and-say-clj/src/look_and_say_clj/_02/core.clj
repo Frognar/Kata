9a12eefbb94861aa451377725b1f9ac128847414
Frognar <sebastian.przyszlak@gmail.com>
2022-10-30 16:16:19 +0100

    '21' -> '1211' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/_02/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/_02/core.clj
@@ -7,5 +7,5 @@
     (if (= (count input) 2)
       (if (= (first input) (second input))
         (str (inc dig_count) (first input))
-        (str dig_count (first input) dig_count (second input)))
+        (str/join (conj result (str dig_count (first input) dig_count (second input)))))
       (str/join (conj result (str dig_count input))))))
