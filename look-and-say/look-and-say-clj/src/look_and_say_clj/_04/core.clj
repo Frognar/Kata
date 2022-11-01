78a477e07fc62d5e6046f94f95e203d340f2e0e6
Frognar <sebastian.przyszlak@gmail.com>
2022-11-01 14:21:47 +0100

    '21' -> '1211' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/_04/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/_04/core.clj
@@ -5,6 +5,6 @@
         count 1]
     (if (not (nil? input))
       (if (= current (first input))
-        (str (inc count) current)
+        (str (inc count) (first input))
         (str count current count (first input)))
       (str count current))))
