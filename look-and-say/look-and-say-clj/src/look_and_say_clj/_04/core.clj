f88f68a5bf490acfef4e5ac93d5e2dec19a1cf62
Frognar <sebastian.przyszlak@gmail.com>
2022-11-01 14:23:28 +0100

    '21' -> '1211' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/_04/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/_04/core.clj
@@ -1,10 +1,12 @@
-(ns look-and-say-clj._04.core)
+(ns look-and-say-clj._04.core
+  (:require [clojure.string :as str]))
 
 (defn look-and-say [input]
-  (loop [[current & input] input
+  (loop [result []
+         [current & input] input
           count 1]
     (if (not (nil? input))
       (if (= current (first input))
-        (recur input (inc count))
+        (recur result input (inc count))
         (str count current count (first input)))
-      (str count current))))
+      (str/join (conj result (str count current))))))
