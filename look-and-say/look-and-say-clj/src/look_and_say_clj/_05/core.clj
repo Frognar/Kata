43956f8456988af823936657f2aff466f9dffe7f
Frognar <sebastian.przyszlak@gmail.com>
2022-11-02 05:38:47 +0100

    '21' -> '1211' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/_05/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/_05/core.clj
@@ -1,8 +1,9 @@
 (ns look-and-say-clj._05.core)
 
 (defn look-and-say [input]
-  (if (not (nil? (second input)))
-    (if (= (first input) (second input))
-      (str 2 (first input))
-      (str 1 (first input) 1 (second input)))
-    (str 1 (first input))))
+  (let [current (first input)]
+    (if (not (nil? (second input)))
+      (if (= current (second input))
+        (str 2 (first input))
+        (str 1 (first input) 1 (second input)))
+      (str 1 (first input)))))
