c36c88a29b3d2a89f11ad8ce3e3591ab081cdeed
Frognar <sebastian.przyszlak@gmail.com>
2022-11-02 05:45:05 +0100

    '111221' -> '312211'

--- look-and-say/look-and-say-clj/spec/look_and_say_clj/_05/core_spec.clj
+++ look-and-say/look-and-say-clj/spec/look_and_say_clj/_05/core_spec.clj
@@ -11,6 +11,8 @@
               (should= "1211" (look-and-say "21")))
           (it "'1211' -> '111221'"
               (should= "111221" (look-and-say "1211")))
+          (it "'111221' -> '312211'"
+              (should= "312211" (look-and-say "111221")))
           )
 
 (run-specs)
