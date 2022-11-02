43b11f9481be7e434042a03b1bef09964b96d27f
Frognar <sebastian.przyszlak@gmail.com>
2022-11-02 05:38:53 +0100

    '21' -> '1211' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/_05/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/_05/core.clj
@@ -4,6 +4,6 @@
   (let [current (first input)]
     (if (not (nil? (second input)))
       (if (= current (second input))
-        (str 2 (first input))
+        (str 2 current)
         (str 1 (first input) 1 (second input)))
       (str 1 (first input)))))
