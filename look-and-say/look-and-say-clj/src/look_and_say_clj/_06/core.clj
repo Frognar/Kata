da8f06fd2f0dade8d2f5b8a518a8b48cbda87d67
Frognar <sebastian.przyszlak@gmail.com>
2022-11-03 05:47:37 +0100

    '1', 1 -> '1;11' reformat

--- look-and-say/look-and-say-clj/src/look_and_say_clj/_06/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/_06/core.clj
@@ -15,7 +15,8 @@
 
 (defn look-and-say-sequence [start depth]
   (let [result [start]
-        current start]
+        current start
+        depth depth]
     (if (> depth 0)
       (let [current (look-and-say current)]
         (str/join ";" (conj result current)))
