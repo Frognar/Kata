48589545c4a8833e7580ee3d2ffd9acfe6bf61ec
Frognar <sebastian.przyszlak@gmail.com>
2022-11-04 05:33:51 +0100

    '21' -> '1211' refactor

--- look-and-say/look-and-say-clj/src/look_and_say_clj/_07/core.clj
+++ look-and-say/look-and-say-clj/src/look_and_say_clj/_07/core.clj
@@ -8,5 +8,5 @@
     (if (not (nil? (second input)))
       (if (= current (second input))
         (say (inc count) current)
-        (str count (first input) count (second input)))
+        (str count current count (second input)))
       (say count (first input)))))
